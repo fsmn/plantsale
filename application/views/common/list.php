61d63319a0ef2fe1d023c4300642898e70366a9c
Chris Dart <chrisdart@cerebratorium.com>
2013-02-27 11:22:55 -0600

    Row adjustments for common listing

--- application/views/common/list.php
+++ application/views/common/list.php
@@ -8,20 +8,15 @@
 	<?if($full_list):?><thead>
 		<tr>
 			<th>Name</th>
-		</tr>
-		<tr>
+
 			<th>Species</th>
-		</tr>
-		<tr>
+	
 			<th>Genus</th>
-		</tr>
-		<tr>
+	
 			<th>Category</th>
-		</tr>
-		<tr>
+	
 			<th>Description</th>
-		</tr>
-		<tr>
+	
 			<th></th>
 		</tr>
 	</thead>
@@ -32,28 +27,22 @@
 			<td><span class="common-name common-edit-row"
 				id="cnid_<?=$name->id;?>"><?=$name->name;?> </span>
 			</td>
-		</tr>
-		<tr>
+	
 			<td><span class="common-species common-edit-row"
 				id="csid_<?=$name->id;?>"><?=$name->species;?> </span>
 			</td>
-		</tr>
-		<tr>
+		
 			<td><span class="common-genus common-edit-row"
 				id="cgid_<?=$name->id;?>"><?=$name->genus;?> </span>
 			</td>
-		</tr>
-		<tr>
+		
 			<td><span class="common-category common-edit-row"
 				id="ccid_<?=$name->id;?>"><?=$name->category;?> </span>
 			</td>
-		</tr>
-		<tr>
-			<td><span class="common-description common-edit-row"
+					<td><span class="common-description common-edit-row"
 				id="cdid_<?=$name->id;?>"><?=$name->description;?> </span>
 			</td>
-		</tr>
-		<tr>
+		
 			<td><span class="button" id="id_<?=$name->id;?>">View Details</span>
 			</td>
 		</tr>

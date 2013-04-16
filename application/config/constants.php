01f2338ee9a39d6f6fde087d3636f930e2efd012
Chris Dart <chrisdart@cerebratorium.com>
2013-04-16 11:43:22 -0500

    image directory setup

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,9 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+/*Images directory */
+define("IMAGE_PATH", $_SERVER['DOCUMENT_ROOT'] . "/images");
+
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */

3c93a29da022085706c37227f4b6a6e4646ff9c7
Chris Dart <technology@fsmn.org>
2013-04-17 11:51:59 -0500

    flat cost/plant cost calculation script.

--- js/order.js
+++ js/order.js
@@ -16,4 +16,16 @@ $(document).ready(function(){
 		});
 		
 	});
+	
+	$("input[name='flat_size'],input[name='flat_cost'],input[name='plant_cost']").live("change",function(){
+		flat_size = $("input[name='flat_size']").val();
+		flat_cost = $("input[name='flat_cost']").val();
+		plant_cost = $("input[name='plant_cost']").val();
+		if(flat_cost && !plant_cost){
+			$("input[name='plant_cost']").val( parseFloat(flat_cost) / parseFloat(flat_size) );
+		}else if(!flat_cost && plant_cost){
+			$("input[name='flat_cost']").val(parseFloat(flat_size) * parseFloat(plant_cost));
+		}
+		console.log(flat_cost);
+	});
 });

1db6a62ecb95f6cb18fe5fc44881b0527c2300d7
Chris Dart <technology@fsmn.org>
2014-04-30 13:00:34 -0500

    Minor css adjustments to welcome page

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -12,17 +12,17 @@
 </div>
 <h3>Totals</h3>
 
-<div style="width:350px;margin:0 auto; float: left;">
+<div style="width:250px;margin:0 auto; float: left;">
 <?=create_button_bar(array(array("selection"=>"order","text"=>"Show Order Totals","class"=>"button show-order-totals")));?>
 <div id="order-totals">
 </div>
 </div>
-<div style="float:left; width: 350px;">
+<div style="float:left; width: 250px;">
 <?=create_button_bar(array(array("selection"=>"variety","text"=>"Show Category Totals", "class"=>"button show-category-totals")));?>
 <div id="category-totals">
 </div>
 </div>
-<div style="float:left; width: 350px">
+<div style="float:left; width: 250px">
 <?=create_button_bar(array(array("selection"=>"variety","text"=>"Show Flat Totals", "class"=>"button show-flat-totals")));?>
 <div id="flat-totals">
 </div>

f260d6dcde3123d6bbfd4301241a43da8147ef2d
Chris Dart <technology@fsmn.org>
2014-05-22 11:35:30 -0500

    Minor adjustment to prevent user confusion

--- application/views/variety/menu.php
+++ application/views/variety/menu.php
@@ -2,7 +2,7 @@
 if(DB_ROLE == 1){
 $buttons["edit"] = array("text"=>"Edit","class"=>"button edit variety-edit","id"=>"edit-variety_$variety->id","href"=>site_url("variety/edit/?id=$variety->id"),"selection"=>"home");
 }
-$buttons["print-options"] = array("text"=>"Print","class"=>"button print variety-print-options","id"=>"print-options_$variety->id","href"=>site_url("variety/print_options/$variety->id"),"selection"=>"home");
+$buttons["print-options"] = array("text"=>"Print","class"=>"button print variety-print-options","id"=>"print-options_$variety->id","href"=>site_url("variety/print_options/$variety->id"),"selection"=>"home","target"=>"_blank");
 if(DB_ROLE == 1){
 	$buttons["delete"] = array("text"=>"Delete","class"=>"button delete variety-delete","id"=>"delete-variety_$variety->id","type"=>"span","selection"=>"home");
 }

385fae977f73d7cf25b96258a9c4a645721a0755
Chris Dart <chrisdart@cerebratorium.com>
2014-09-14 20:35:01 -0500

    Arrangement of order search layout

--- application/views/order/search.php
+++ application/views/order/search.php
@@ -12,15 +12,17 @@
 <p><label for="flat_size">Flat Size</label>&nbsp;<input type="number" name="flat_size" style="width:3em;" value="<?php echo get_cookie("flat_size"); ?>"/></p>
 <p><label for="year">Year</label>&nbsp;<input type="number" name="year" style="width:4em" value="<?php echo get_cookie("sale_year");?>"/></p>
 <p>
-<label for="show_fields">Show Inventory Fields</label>
 <input type="checkbox" name="is_inventory" id="is_inventory" value=1/>
+<label for="show_fields">Show Inventory Fields</label>
+</p>
+<p>
+<input type="checkbox" value="1" name="show_last_only"/>
+<label for="show_last_only">Hide plants that already have an order for the next plant sale</label>
 </p>
 <div id="sort-block">
 <? $this->load->view("order/sort");?>
 </div>
-<p>
-<input type="checkbox" value="1" name="show_last_only"/>Hide plants that already have an order for the next plant sale
-</p>
+
 <p><input type="submit" value="Search"/></p>
 
 </form>

3a66836237db86da959e5c098eb9aeb029985aa6
Chris Dart <technology@fsmn.org>
2014-11-14 17:00:13 -0600

    added permissions to image-model

--- application/models/image_model.php
+++ application/models/image_model.php
@@ -1,131 +1,126 @@
 <?php
 
-if (! defined ( 'BASEPATH' ))
-	exit ( 'No direct script access allowed' );
-class Image_Model extends CI_Model {
-	var $variety_id;
-	var $image_source;
-	var $image_name;
-	var $image_type;
-	var $image_size;
-	var $image_path;
-	var $rec_modifier;
-	var $rec_modified;
+if (! defined('BASEPATH')) exit('No direct script access allowed');
 
-	function __construct() {
+class Image_Model extends MY_Model
+{
+    var $variety_id;
+    var $image_source;
+    var $image_name;
+    var $image_type;
+    var $image_size;
+    var $image_path;
+    var $rec_modifier;
+    var $rec_modified;
 
-		parent::__construct ();
-	
-	}
+    function __construct ()
+    {
+        parent::__construct();
+    }
 
-	function prepare_variables($image_data) {
+    function prepare_variables ($image_data)
+    {
+        $variables = array(
+                "variety_id",
+                "image_source"
+        );
+        for ($i = 0; $i < count($variables); $i ++) {
+            $my_variable = $variables[$i];
+            if ($this->input->post($my_variable)) {
+                $this->$my_variable = $this->input->post($my_variable);
+            }
+        }
 
-		$variables = array (
-				"variety_id",
-				"image_source" 
-		);
-		for($i = 0; $i < count ( $variables ); $i ++) {
-			$my_variable = $variables [$i];
-			if ($this->input->post ( $my_variable )) {
-				$this->$my_variable = $this->input->post ( $my_variable );
-			}
-		}
-		
-		if (array_key_exists ( "file_name", $image_data )) {
-			$this->image_name = $image_data ["file_name"];
-			$this->image_path = "files/" . $this->image_name;
-		}
-		
-		if (array_key_exists ( "file_type", $image_data )) {
-			$this->image_type = $image_data ["file_type"];
-		}
-		
-		if (array_key_exists ( "file_size", $image_data )) {
-			$this->image_size = $image_data ["file_size"];
-		}
-		
-		$this->rec_modified = mysql_timestamp ();
-		$this->rec_modifier = $this->ion_auth->user ()->row ()->id;
-	
-	}
+        if (array_key_exists("file_name", $image_data)) {
+            $this->image_name = $image_data["file_name"];
+            $this->image_path = "files/" . $this->image_name;
+        }
 
-	function insert($variety_id, $image_data) {
+        if (array_key_exists("file_type", $image_data)) {
+            $this->image_type = $image_data["file_type"];
+        }
 
-		$this->prepare_variables ( $image_data );
-		$this->db->insert ( 'image', $this );
-		return $this->db->insert_id ();
-	
-	}
+        if (array_key_exists("file_size", $image_data)) {
+            $this->image_size = $image_data["file_size"];
+        }
 
-	function get($id) {
+        $this->rec_modified = mysql_timestamp();
+        $this->rec_modifier = $this->ion_auth->user()->row()->id;
+    }
 
-		$this->db->where ( 'id', $id );
-		$this->db->from ( 'image' );
-		$result = $this->db->get ()->row ();
-		return $result;
-	
-	}
+    function insert ($variety_id, $image_data)
+    {
+        $this->prepare_variables($image_data);
+        return $this->_insert("image");
+    }
 
-	function get_for_variety($variety_id) {
+    function get ($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->from('image');
+        $result = $this->db->get()->row();
+        return $result;
+    }
 
-		$this->db->where ( "variety_id", $variety_id );
-		$this->db->from ( "image" );
-		$result = $this->db->get ()->result ();
-		return $result;
-	
-	}
+    function get_for_variety ($variety_id)
+    {
+        $this->db->where("variety_id", $variety_id);
+        $this->db->from("image");
+        $result = $this->db->get()->result();
+        return $result;
+    }
 
-	function get_all($variety_id) {
+    function get_all ($variety_id)
+    {
+        $this->db->where('variety_id', $variety_id);
+        $this->db->from('image');
+        $this->db->order_by('image_display_name');
+        $result = $this->db->get()->result();
+        return $result;
+    }
 
-		$this->db->where ( 'variety_id', $variety_id );
-		$this->db->from ( 'image' );
-		$this->db->order_by ( 'image_display_name' );
-		$result = $this->db->get ()->result ();
-		return $result;
-	
-	}
+    function delete ($id)
+    {
+        if($this->ion_auth->in_group(array(1,2))){
+        $image = $this->get($id);
+        $filename = $_SERVER['DOCUMENT_ROOT'] . "files/" . $image->image_name;
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
+        $id_array = array(
+                'id' => $id
+        );
+        $this->db->delete('image', $id_array);
+        }else{
+            return FALSE;
+        }
+    }
 
-	function delete($id) {
+    function fetch_values ($fields, $image_fields = null)
+    {
+        $this->db->from('image');
+        $this->db->distinct();
 
-		$image = $this->get ( $id );
-		$filename = $_SERVER ['DOCUMENT_ROOT'] . "files/" . $image->image_name;
-		if (file_exists ( $filename )) {
-			unlink ( $filename );
-		}
-		$id_array = array (
-				'id' => $id 
-		);
-		$this->db->delete ( 'image', $id_array );
-	
-	}
+        if (is_array($fields)) {
+            foreach ($fields as $field) {
+                $this->db->select($field);
+            }
+        } else {
+            $this->db->select($fields);
+        }
 
-	function fetch_values($fields, $image_fields = null) {
+        if ($image_fields) {
+            if (is_array($image_fields)) {
+                foreach ($image_fields as $image) {
+                    $this->db->order_by($image);
+                }
+            } else {
+                $this->db->order_by($image_fields);
+            }
+        }
 
-		$this->db->from ( 'image' );
-		$this->db->distinct ();
-		
-		if (is_array ( $fields )) {
-			foreach ( $fields as $field ) {
-				$this->db->select ( $field );
-			}
-		} else {
-			$this->db->select ( $fields );
-		}
-		
-		if ($image_fields) {
-			if (is_array ( $image_fields )) {
-				foreach ( $image_fields as $image ) {
-					$this->db->order_by ( $image );
-				}
-			} else {
-				$this->db->order_by ( $image_fields );
-			}
-		}
-		
-		$query = $this->db->get ();
-		$result = $query->result ();
-		return $result;
-	
-	}
-
+        $query = $this->db->get();
+        $result = $query->result();
+        return $result;
+    }
 }

97b5d1b9812facd87dc07b66c00d64d356e7134e
Chris Dart <technology@fsmn.org>
2015-01-21 19:08:47 -0600

    Change batch update button to edit style

--- application/views/order/full_list.php
+++ application/views/order/full_list.php
@@ -47,7 +47,7 @@ Found Count: <strong><?=count($orders);?> Orders</strong>
 <? $buttons[] = array("text"=>"Full Export","title"=>"Export all the fields using the current sort","class"=>array("button","array"), "href"=>$_SERVER['REQUEST_URI']. "&export=true" );
 $buttons[] = array("text"=>"Grower Export","title"=>"Export grower fields using a special grower export","class"=>array("button","array"), "href"=>$_SERVER['REQUEST_URI']. "&export=true&export_type=grower" );
 if(IS_ADMIN){
-$buttons[] = array("text"=>"Batch Update","title"=>"Batch Update values for all the listed orders.","class"=>array("button","batch-update-orders"));
+$buttons[] = array("text"=>"Batch Update","title"=>"Batch Update values for all the listed orders.","class"=>array("button","batch-update-orders","edit"));
 }
 print create_button_bar($buttons);
 ?>

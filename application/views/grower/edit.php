11c4faa079ca7a238ecdc284909830893ecc33b4
Chris Dart <technology@fsmn.org>
2015-01-27 15:41:48 -0600

    Bug fix. Fixed grower form.

--- application/views/grower/edit.php
+++ application/views/grower/edit.php
@@ -4,24 +4,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $countries = array("USA"=>"USA","Canada"=>"Canada");
 // view.php Chris Dart Jan 8, 2015 5:30:58 PM chrisdart@cerebratorium.com
 $fields = array(
-        "grower_name" => array("label"=>"Name"),
-        "street_address" => array("label"=>"Street Address"),
-        "po_box" => array("label"=>"PO Box"),
-        "city" => array("label"=>"City"),
-        "state" => array("label"=>"State"),
-        "zip" => array("label"=>"Zip"),
-        "country" => array("label"=>"Country","type"=>"dropdown","options"=>$countries),
-        "website" => array("label"=>"Website"),
-        "email" => array("label"=>"Email"),
-        "phone" => array("label"=>"Phone"),
-        "fax" => array("label"=>"Fax"),
+        "grower_name" => array("name"=>"grower_name","label"=>"Name"),
+        "street_address" => array("name"=>"street_address","label"=>"Street Address"),
+        "po_box" => array("name"=>"po_box","label"=>"PO Box"),
+        "city" => array("name"=>"city","label"=>"City"),
+        "state" => array("name"=>"state","label"=>"State"),
+        "zip" => array("name"=>"zip","label"=>"Zip"),
+        "country" => array("name"=>"country","label"=>"Country","type"=>"dropdown","options"=>$countries),
+        "website" => array("name"=>"website","label"=>"Website"),
+        "email" => array("name"=>"email","label"=>"Email"),
+        "phone" => array("name"=>"phone","label"=>"Phone"),
+        "fax" => array("name"=>"fax","label"=>"Fax"),
 );
 foreach ($fields as $field ) {
 if(array_key_exists("type",$field) && $field["type"] == "dropdown"){
-    $output[] = sprintf("<p><label for='%s'>%s:&nbsp;</label>%s</p>",$field, $field["label"],form_dropdown($field,$field["options"],"USA"));
+    $output[] = sprintf("<p><label for='%s'>%s:&nbsp;</label>%s</p>",$field["name"], $field["label"],form_dropdown($field["name"],$field["options"],"USA"));
 }else{
 
-    $output[] = sprintf("<p>%s</p>", create_input($grower, $field, $field["label"], $field));
+    $output[] = sprintf("<p>%s</p>", create_input($grower, $field["name"], $field["label"], $field["name"]));
 }
 }
 ?>

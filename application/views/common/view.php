5cad18d9b0651a5aec4823d543be3b7c8e9162b1
Chris Dart <technology@fsmn.org>
2015-01-27 16:19:45 -0600

    Redesign common display for balance and readability

--- application/views/common/view.php
+++ application/views/common/view.php
@@ -3,13 +3,61 @@
 	<?=edit_field("name", $common->name, "","common",$common->id,array("envelope"=>"span"));?>
 
 </h2>
-<? if(IS_EDITOR):?>
-<? $buttons[] = array("selection" => "common" , "text" => "Edit", "class" => array("button","edit","common-edit"), "id" => "ec_$common->id", "type" => "span", "title" => "Edit this record");?>
-<? $buttons[] = array("selection"=>"common","text"=>"Delete","class"=>array("button","delete","delete-common"), "id"=>"delete-common_$common->id","type"=>"span","title"=>"Delete this Common");?>
-<?=create_button_bar($buttons);?>
-<? endif; ?>
-<div class="grouping" id="common">
-	<input type="hidden" name="id" id="id" value="<?=$common->id;?>" />
+<div
+	class="grouping column left"
+	style="min-width: 400px;"
+	id="common">
+<?
+
+if (IS_EDITOR) {
+    $buttons["edit_common"] = array(
+            "selection" => "common",
+            "text" => "Edit",
+            "class" => array(
+                    "button",
+                    "edit",
+                    "common-edit"
+            ),
+            "id" => "ec_$common->id",
+            "type" => "span",
+            "title" => "Edit this record"
+    );
+
+    $buttons["add_variety"] = array(
+            "selection" => "variety",
+            "text" => "Add a variety",
+            "class" => array(
+                    "button",
+                    "new",
+                    "variety-create"
+            ),
+            "id" => sprintf("common-id_%s", $common->id),
+            "type" => "span",
+            "title" => "add a new variety"
+    );
+    if (empty($varieties)) {
+        $buttons["delete_common"] = array(
+                "selection" => "common",
+                "text" => "Delete",
+                "class" => array(
+                        "button",
+                        "delete",
+                        "delete-common"
+                ),
+                "id" => "delete-common_$common->id",
+                "type" => "span",
+                "title" => "Delete this Common"
+        );
+
+    }
+    print create_button_bar($buttons);
+
+}?>
+	<input
+		type="hidden"
+		name="id"
+		id="id"
+		value="<?=$common->id;?>" />
 
 	<?=edit_field("genus", $common->genus, "Genus","common",$common->id);?>
 	<?=edit_field("category_id", $common->category, "Category","common",$common->id, array("envelope"=>"p","class"=>"category-dropdown"));?>
@@ -20,31 +68,15 @@
 	<?=edit_field("sunlight",$common->sunlight, "Sunlight Requirements","common",$common->id,array("class"=>"multiselect","attributes"=>"menu='sunlight'","format"=>"multiselect"));?>
 
 </div>
-
 <?
-$new_variety_buttons[] = array (
-					"selection" => "variety",
-					"text" => "Add a variety",
-					"class" => array (
-							"button",
-							"new",
-							"variety-create"
-					),
-					"id" => sprintf ( "common-id_%s", $common->id ),
-					"type" => "span",
-					"title" => "add a new variety"
-			);
-
 if (IS_EDITOR) {
-    print create_button_bar ($new_variety_buttons);
+    print create_button_bar(array(
+            $buttons["add_variety"]
+    ));
 }
 
 ?>
+<div class="column-right">
 <? $this->load->view("variety/list");?>
-<?
-
-
-if (IS_EDITOR) {
-	print create_button_bar ($new_variety_buttons);
-}
+</div>
 

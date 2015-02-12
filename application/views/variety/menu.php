f3c69b546bf2b2067bf1df3527adbdd869422443
Chris Dart <chrisdart@cerebratorium.com>
2015-02-11 21:29:51 -0600

    Change class for printing single variety item

--- application/views/variety/menu.php
+++ application/views/variety/menu.php
@@ -2,8 +2,8 @@
 if(IS_EDITOR){
 $buttons["edit"] = array("text"=>"Edit","class"=>"button edit variety-edit","id"=>"edit-variety_$variety->id","href"=>site_url("variety/view/$variety->id"),"selection"=>"home");
 }
-$buttons["print-tabloid"] = array("text"=>"Print Tabloid","class"=>"button print variety-print-tabloid","id"=>"print-tabloid_$variety->id","href"=>site_url("variety/print_one/$variety->id/tabloid"),"target"=>"_blank");
-$buttons["print-statement"] = array("text"=>"Print Statement","class"=>"button print variety-print-statement","id"=>"print-statement_$variety->id","href"=>site_url("variety/print_one/$variety->id/statement"),"target"=>"_blank");
+$buttons["print-tabloid"] = array("text"=>"Print Tabloid","class"=>"button print variety-print-tabloid-one","id"=>"print-tabloid_$variety->id","href"=>site_url("variety/print_one/$variety->id/tabloid"),"target"=>"_blank");
+$buttons["print-statement"] = array("text"=>"Print Statement","class"=>"button print variety-print-statement-one","id"=>"print-statement_$variety->id","href"=>site_url("variety/print_one/$variety->id/statement"),"target"=>"_blank");
 
 if(IS_EDITOR){
 	$buttons["delete"] = array("text"=>"Delete","class"=>"button delete variety-delete","id"=>"delete-variety_$variety->id","type"=>"span","selection"=>"home");

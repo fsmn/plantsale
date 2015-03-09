881295ad2ba9fa9f321ea3bde43e1caa5ccf1953
Chris Dart <technology@fsmn.org>
2015-03-09 15:01:47 -0500

    Whiney autocomplete file
    Is this even in use? I should check.

--- application/views/page/autocomplete.php
+++ application/views/page/autocomplete.php
@@ -1,3 +1,5 @@
 <?php  defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<?php echo create_button_bar(array(array("text"=>"Turn Autocomplete Off","class"=>array("link","autocomplete-off"))));?>
+
+$buttons[] = array("text"=>"Turn Autocomplete Off","class"=>array("link","autocomplete-off"));
+
+echo create_button_bar($buttons);

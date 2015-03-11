acd63f323698d9749890654085317c42d3794184
Chris Dart <technology@fsmn.org>
2015-03-10 20:52:21 -0500

    Minor fix to printing letter size documents

--- application/views/variety/menu.php
+++ application/views/variety/menu.php
@@ -4,7 +4,7 @@ $buttons["edit"] = array("text"=>"Edit","class"=>"button edit variety-edit","id"
 }
 $buttons["print-tabloid"] = array("text"=>"Print Tabloid","class"=>"button print variety-print-tabloid-one","id"=>"print-tabloid_$variety->id","href"=>site_url("variety/print_one/$variety->id/tabloid"),"target"=>"_blank");
 $buttons["print-statement"] = array("text"=>"Print Statement","class"=>"button print variety-print-statement-one","id"=>"print-statement_$variety->id","href"=>site_url("variety/print_one/$variety->id/statement"),"target"=>"_blank");
-$buttons["print-letter"] = array("text"=>"Print Letter","class"=>"button print variety-print-letter","href"=>site_url("variety/print_result/letter"), "target"=>"_blank");
+$buttons["print-letter"] = array("text"=>"Print Letter","class"=>"button print variety-print-letter-one","href"=>site_url("variety/print_one/$variety->id/letter"), "target"=>"_blank");
 
 if(IS_EDITOR){
 	$buttons["delete"] = array("text"=>"Delete","class"=>"button delete variety-delete","id"=>"delete-variety_$variety->id","type"=>"span","selection"=>"home");

937a48b232e435f9888f2f710e9ec74e7b9012f5
Chris Dart <technology@fsmn.org>
2015-03-27 21:08:12 -0500

    More tests

--- application/controllers/variety.php
+++ application/controllers/variety.php
@@ -73,7 +73,7 @@ class Variety extends MY_Controller
                         "thumbnail"
                 );
         foreach($formats as $format){
-            $this->resize_image($id, "letter");
+            $this->resize_image($id, "tabloid");
         }
 
         $variety = $this->variety->get($id);

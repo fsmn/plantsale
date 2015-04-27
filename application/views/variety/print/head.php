ba8b88010046c7396f6111a47ff230904e1ea3b7
Chris Dart <technology@fsmn.org>
2015-04-27 10:26:24 -0500

    Code cleanup after edits to sign interface

--- application/views/variety/print/head.php
+++ application/views/variety/print/head.php
@@ -1,29 +1,29 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-
 <link
 	type="text/css"
 	rel="stylesheet"
 	media="all"
 	href="<?=base_url("css/$format.css")?>" />
-
-	<style type="text/css">
+<style type="text/css">
 @media print {
 	.no-print {
 		display: none;
 	}
-
 }
+
 a {
 	text-decoration: none;
 	color: #000;
 }
+
 a:hover {
 	text-decoration: underline;
 }
 
-
-
+.price:hover,.pot-size:hover {
+	background-color: yellow;
+	cursor: pointer;
+}
 </style>
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-
-	<script src="<?=base_url("js/signs.js");?>"></script>
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src="<?=base_url("js/signs.js");?>"></script>
--- js/signs.js
+++ js/signs.js
@@ -1,25 +1,23 @@
 $(document).on("click",".price, .pot-size", function(){
-var my_field = this;
-var my_id = my_field.id;
-var my_class = $(my_field).attr("class");
-var my_value = $(my_field).html();
-
-$(my_field).removeClass(my_class);
-$(my_field).addClass(my_class + "_field");
-
-$(my_field).html("<input type='text' value='" + my_value + "'/>");
-$(my_field).children("input").focus();
+	var my_field = this;
+	var my_class = $(my_field).attr("class");
+	var my_value = $(my_field).html();
+	
+	$(my_field).removeClass(my_class);
+	$(my_field).addClass(my_class + "_field");
+	
+	$(my_field).html("<input type='text' value='" + my_value + "'/>");
+	$(my_field).children("input").focus();
 });
 
 $(document).on("blur",".price_field, .pot-size_field", function(){
 	var my_field = this;
 	var my_child = $(this).children("input");
-var my_value = my_child.val();
-var my_class = $(my_field).attr("class");
-console.log(my_value);
-$(my_field).removeClass(my_class);
-var new_class = my_class.split("_")[0];
-$(my_field).addClass(new_class);
-$(my_field).html(my_value);
+	var my_value = my_child.val();
+	var my_class = $(my_field).attr("class");
+	$(my_field).removeClass(my_class);
+	var new_class = my_class.split("_")[0];
+	$(my_field).addClass(new_class);
+	$(my_field).html(my_value);
 });
 

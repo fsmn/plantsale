d30bbc8d4ad61bc5ce51f0e58537a8515c3243f0
chrisdart <chrisd@fsmn.org>
2015-12-01 15:48:52 -0600

    More tweaks to inventory system.

--- application/views/inventory/search.php
+++ application/views/inventory/search.php
@@ -7,9 +7,10 @@
 			<input type="text" class="form-control" name="catalog_number" id="catalog-number" value="<?php echo $catalog_number; ?>"  />
 		</div>
 		<div class="form-group">
+<div class="field-wrapper col-sm-5 col-md-2">
 
 			<input type="submit" value="Search" class="btn btn-success btn-lg search" />
-
+</div>
 		</div>
 	</form>
 	<div id="catalog_number_error"></div>

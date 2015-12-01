d4c04db1379bf0313c1a1ab5d282c31f7142f5a5
chrisdart <chrisd@fsmn.org>
2015-12-01 15:35:47 -0600

    updates to inventory display and functionality.

--- application/views/inventory/edit.php
+++ application/views/inventory/edit.php
@@ -3,30 +3,28 @@
 function form_group($field_name, $field_value, $label, $field_type = "text", $class=FALSE)
 {
 	if($label){
-	$label_string = sprintf ( "<label for='%s' class='control-label col-sm-2'>%s</label>", $field_name, $label );
+	$label_string = sprintf ( "<label for='%s' class='control-label'>%s</label>", $field_name, $label );
 	}else{
 		$label_string = "";
 	}
 	$field = sprintf ( "<input type='%s' class='form-control number %s' id='%s' name='%s' value='%s'/>", $field_type, $class, $field_name,$field_name, $field_value);
-	$output = sprintf ( "<div class='form-group'>%s<div class='field-wrapper col-sm-10'>%s</div></div>", $label_string, $field );
+	$output = sprintf ( "<div class='form-group'>%s<div class='field-wrapper'>%s</div></div>", $label_string, $field );
 	return $output;
 }
 
 $edit_fields = array (
-		"received_presale" => array("label"=>"Rec'd Thurs","type"=>"text","day"=>"Thursday"),
-		"sellout_friday" => array("label"=>"Sellout Time Fri","type"=>"time","day"=>"Friday"),
-		"remainder_friday"=>array("label"=>"Rem. Fri","type"=>"text","day"=>"Friday"),
-		"received_midsale"=> array("label"=>"Rec'd Sat","type"=>"text","day"=>"Saturday"),
-		"sellout_saturday"=>array("label"=>"Sellout Sat","type"=>"time","day"=>"Saturday"),
-		"remainder_sunday"=>array("label"=>"Rem. Sun","type"=>"text","day"=>"Sunday"),
+		"received_presale" => array("label"=>"Received Thursday","type"=>"text","day"=>"Thursday"),
+		"sellout_friday" => array("label"=>"Sellout Time Friday","type"=>"time","day"=>"Friday"),
+		"remainder_friday"=>array("label"=>"Remainder Friday","type"=>"text","day"=>"Friday"),
+		"received_midsale"=> array("label"=>"Received Saturday","type"=>"text","day"=>"Saturday"),
+		"sellout_saturday"=>array("label"=>"Sellout Saturday","type"=>"time","day"=>"Saturday"),
+		"remainder_sunday"=>array("label"=>"Remainder Sundat","type"=>"text","day"=>"Sunday"),
 		"count_dead"=>array("label"=>"Dead Count","type"=>"text","day"=>"Sunday"),
-		
-		
 );
 
 
 ?>
-<form name="inventory-editor" id="inventory-editor" class="form-horizontal" method="post" action="<?php echo site_url("inventory/check");?>">
+<form name="inventory-editor" id="inventory-editor" class="form" method="post" action="<?php echo site_url("inventory/check");?>">
 <input type="hidden" name="id" value="<?php echo $item->id;?>"/>
 <input type="hidden" name="step" value="<?php echo $step;?>"/>
 <input type="hidden" name="catalog_number" value="<?php echo $item->catalog_number;?>"/>

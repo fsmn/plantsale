011f3fab3d234cb7e836ffbe2ad9dc2d66d24f7c
chrisdart <chrisd@fsmn.org>
2016-01-08 15:54:47 -0600

    Refactoring login screen.

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,24 +1,21 @@
 <div class="login">
 <h2>Friends School Plantsale Database</h2>
-<p><?php echo lang('login_subheading');?></p>
+<p><a style="font-weight:bold;" href="forgot_password">Forgot your password?</a></p>
 <?php if(isset($message)):?>
 <div class="message alert"><?php echo $message;?></div>
 <?php endif;?>
 <?php echo form_open("auth/login");?>
 
   <p>
-    <label for="identity">Email/Username: </label>
-    <?php echo form_input($identity);?>
+    <?php echo form_input($identity,"","placeholder='email address'");?>
   </p>
 
   <p>
-    <label for='password'>Password: </label>
-    <?php echo form_input($password);?>
+    <?php echo form_input($password,"","placeholder='password'");?>
   </p>
 
   <p><?php echo form_submit('submit', "Login", "class='button'");?></p>
 
 <?php echo form_close();?>
 
-<p><a style="font-weight:bold;" href="forgot_password">Forgot your password?</a></p>
 </div>

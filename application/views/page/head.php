fafb1d50db90c0ce8128cd0095089cbc2b1fcb59
chrisdart <chrisd@fsmn.org>
2016-01-08 14:58:43 -0600

    log out after 4 hours of inactivity.

--- application/views/page/head.php
+++ application/views/page/head.php
@@ -2,6 +2,7 @@
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title><?=$title;?></title>
+<meta http-equiv="refresh" content = "14400; url=<?php echo site_url("auth/logout");?>">
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" media="screen"> -->		
 <link type="text/css" rel="stylesheet" media="all" href="<?=base_url("css/main.css")?>" />
 <link type="text/css" rel="stylesheet" media="all" href="<?=base_url("css/color.css")?>"/>

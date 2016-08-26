f3c0a05c4b107121324e25d581aebf2a5b2eec57
chrisdart <chrisd@fsmn.org>
2016-08-26 13:27:32 -0500

    More database schema updates

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -5,8 +5,9 @@ class Database extends MY_Controller{
 		
 	}
 	function update_database(){
-		$this->db->query("ALTER TABLE `variety` ADD `churn_value` INT  NULL  DEFAULT NULL  AFTER `edit_notes`;
-INSERT INTO `menu` (`id`, `category`, `key`, `value`, `rec_modified`, `rec_modifier`, `rec_created`)
+		$this->db->query("ALTER TABLE `variety` ADD `churn_value` INT  NULL  DEFAULT NULL  AFTER `edit_notes`;");
+		$this->db->query("
+REPLACE INTO `menu` (`id`, `category`, `key`, `value`, `rec_modified`, `rec_modifier`, `rec_created`)
 VALUES
 	(65, 'churn_value', '2', '2', '2016-08-26 13:14:56', NULL, NULL),
 	(66, 'churn_value', '3', '3', '2016-08-26 13:15:05', NULL, NULL),

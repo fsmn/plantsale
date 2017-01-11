1c8ef6e3b79cee2a6b6f4910c121d910984aa9ca
chrisdart <chrisd@fsmn.org>
2017-01-11 10:17:41 -0600

    Update database script

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -5,12 +5,6 @@ class Database extends MY_Controller{
 		
 	}
 	function update_database(){
-		$this->db->query("ALTER TABLE `variety` ADD `churn_value` INT  NULL  DEFAULT NULL  AFTER `edit_notes`;");
-		$this->db->query("
-REPLACE INTO `menu` (`id`, `category`, `key`, `value`, `rec_modified`, `rec_modifier`, `rec_created`)
-VALUES
-	(65, 'churn_value', '2', '2', '2016-08-26 13:14:56', NULL, NULL),
-	(66, 'churn_value', '3', '3', '2016-08-26 13:15:05', NULL, NULL),
-	(67, 'churn_value', '0', 'None', '2016-08-26 13:19:29', NULL, NULL);");
+		$this->db->query("ALTER TABLE `variety` CHANGE `copy_received` `copy_received` VARCHAR(10)  CHARACTER SET utf8  COLLATE utf8_unicode_ci  NULL  DEFAULT 'no'  COMMENT 'yes/no copy has been received';");
 	}
 }

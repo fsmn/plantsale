718223c0df3a6b7aafa9f8fedcee059979284bad
chrisdart <chrisd@fsmn.org>
2017-04-06 12:25:00 -0500

    Spacing seed packets

--- application/views/variety/print/multiple.php
+++ application/views/variety/print/multiple.php
@@ -1,14 +1,23 @@
-<?php defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+<?php
+
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 $i = 1;
 foreach ( $plants as $plant ) {
-if($i %2 == 0 && $i!=1){
-    $plant['row_class'][] = "even";
-}else{
-     $plant['row_class'][] = "odd";
-}
-if($i == 1){
-	$plant['row_class'][] = "first";
-}
-		$this->load->view ( "variety/print/$format", $plant );
-$i++;
+	if ($i % 2 == 0 && $i != 1) {
+		$plant ['row_class'] [] = "even";
+	} else {
+		$plant ['row_class'] [] = "odd";
+	}
+	if ($i == 1) {
+		$plant ['row_class'] [] = "first";
+	}
+	
+	if ($format == "seed_packet" && $i % 2 == 1) {
+		print "<div class='packet-row'>";
+	}
+	$this->load->view ( "variety/print/$format", $plant );
+	if ($format == "seed_packet" && $i % 2 == 0) {
+		print "</div>";
+	}
+	$i ++;
 }

55c46808b71a42f3e34357e26f6b20b29803cd33
chrisdart <chrisd@fsmn.org>
2017-11-10 14:14:15 -0600

    updates in prep for upgrade to CI 3.1.6

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -5,8 +5,7 @@ class Database extends MY_Controller{
 		
 	}
 	function update_database(){
-		//$this->db->query("ALTER TABLE `variety` CHANGE `copy_received` `copy_received` VARCHAR(10)  CHARACTER SET utf8  COLLATE utf8_unicode_ci  NULL  DEFAULT 'no'  COMMENT 'yes/no copy has been received';");
+		$this->db->query("ALTER TABLE user_sessions CHANGE id id varchar(128) NOT NULL;");
 	}
 }
 
-//740524

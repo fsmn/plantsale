a9b622d8193930d06fa09265f99e757a515471da
chrisdart <chrisd@fsmn.org>
2018-06-26 11:15:47 -0500

    fix grower report totals bug

--- application/models/Grower_model.php
+++ application/models/Grower_model.php
@@ -121,22 +121,16 @@ class grower_model extends MY_Model
 
     function get_totals ($id, $year)
     {
-/*         $query = sprintf(
-                "SELECT sum(`o`.`total`) as `total`, `grower`.* FROM (SELECT `grower_id`, (IFNULL(`count_presale`,0) + IFNULL(`count_midsale`,0)) * `flat_cost` as `total` FROM (`orders`) WHERE `year` = '%s' AND `orders`.`grower_id` = '%s' )  as `o` LEFT JOIN `grower` on `grower`.`id` = `o`.`grower_id` GROUP BY `o`.`grower_id`",
-                $year, $id); */
-
         $query = sprintf( "SELECT sum(`o`.`total`) as `total`, `grower`.*,`users`.`first_name`,`users`.`last_name`,
-                `shipping`.`name` AS `shipping_name`,`shipping`.`phone1` AS `shipping_phone1`, `shipping`.`phone2` AS `shipping_phone2`, `shipping`.`email` AS `shipping_email`,
-                `ordering`.`name` AS `ordering_name`,`ordering`.`phone1` AS `ordering_phone1`, `ordering`.`phone2` AS `ordering_phone2`, `ordering`.`email` AS `ordering_email`
+                `shipping`.`name` AS `shipping_name`,`shipping`.`phone1` AS `shipping_phone1`, `shipping`.`phone2` AS `shipping_phone2`, `shipping`.`email` AS `shipping_email`
                 FROM (SELECT `grower_id`, (IFNULL(`count_presale`,0) + IFNULL(`count_midsale`,0)) * `flat_cost` as `total` FROM (`orders`)
                 WHERE `year` = '%s' AND `orders`.`grower_id` = '%s' ) as `o`
                 LEFT JOIN `grower` ON `grower`.`id` = `o`.`grower_id`
                 LEFT JOIN `contact` AS `shipping` ON `shipping`.`grower_id` = `grower`.`id` AND `shipping`.`contact_type` = 'shipping'
-                LEFT JOIN `contact` AS `ordering` ON `ordering`.`grower_id` = `grower`.`id` AND `ordering`.`contact_type` = 'ordering'
         		LEFT JOIN `users` AS `users` ON `grower`.`user_id` = `users`.`id`
                 GROUP BY `o`.`grower_id`",$year, $id);
         $result = $this->db->query($query)->row();
-        //$this->_log("alert");
+        $this->_log("alert");
         return $result;
     }
 }

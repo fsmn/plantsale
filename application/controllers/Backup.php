c1b9ae0104161b882f4421136c65664157511d7f
chrisdart <chrisd@fsmn.org>
2018-10-26 17:12:22 -0500

    Add silent option to backup tables to csv

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -1,68 +1,73 @@
-<?php  defined('BASEPATH') OR exit('No direct script access allowed');
-class Backup extends MY_Controller{
-	
-	
-	function __construct(){
-		parent::__construct();
+<?php
+
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+class Backup extends MY_Controller {
+
+	function __construct()
+	{
+		parent::__construct ();
 	}
-	
-	function index(){
-		$this->load->dbutil();
-		$tables = $this->db->query("SHOW TABLES")->result();
-		$data['target'] = "backup/control";
-		$data['title'] = "Backup Data Tables";
-		foreach($tables as $table => $item){
-			foreach($item as $string){
-				$data['tables'][] = $string;
+
+	function index()
+	{
+		$this->load->dbutil ();
+		$tables = $this->db->query ( "SHOW TABLES" )->result ();
+		$data ['target'] = "backup/control";
+		$data ['title'] = "Backup Data Tables";
+		foreach ( $tables as $table => $item ) {
+			foreach ( $item as $string ) {
+				$data ['tables'] [] = $string;
 			}
-			
 		}
-		$this->load->view('page/index',$data);
-		
+		$this->load->view ( 'page/index', $data );
 	}
-	
-	function backup_table($table){
-		ini_set('memory_limit', '4096M');
-		
-		// Load the DB utility class
-		$this->load->dbutil();
-		//	$dbs = $this->dbutil->list_databases();
-		// Backup your entire database and assign it to a variable
-		$prefs = array('tables'=>array($table));
-		$backup = $this->dbutil->backup($prefs);
-		$filename = sprintf("backup-%s-%s.sql.gz",$table, date("Y-m-d-H-i-s"));
-		$path = sprintf("/tmp/");
-		$temp_file = $path . $filename;
-		// Load the file helper and write the file to your server
-		$this->load->helper('file');
-		write_file($temp_file, $backup);
-		// Load the download helper and send the file to your desktop
-		$this->load->helper('download');
-		force_download($filename, $backup);
-		//*/
-		redirect("/");
+
+	function backup_table($table, $type = "sql")
+	{
+		if ($type == "sql") {
+			ini_set ( 'memory_limit', '4096M' );
+			
+			// Load the DB utility class
+			$this->load->dbutil ();
+			// $dbs = $this->dbutil->list_databases();
+			// Backup your entire database and assign it to a variable
+			$prefs = array (
+					'tables' => array (
+							$table 
+					) 
+			);
+			$backup = $this->dbutil->backup ( $prefs );
+			$filename = sprintf ( "%s-backup-%s.sql.gz", $table, date ( "Y-m-d-H-i-s" ) );
+			$path = sprintf ( "/tmp/" );
+			$temp_file = $path . $filename;
+			// Load the file helper and write the file to your server
+			$this->load->helper ( 'file' );
+			write_file ( $temp_file, $backup );
+			// Load the download helper and send the file to your desktop
+			$this->load->helper ( 'download' );
+			force_download ( $filename, $backup );
+		} elseif ($type == "csv") {
+			$this->_backup_csv ( $table );
+		}
+		redirect ( "/" );
 	}
-	
-	function get_csv($table=NULL){
-		$this->load->dbutil();
+
+	function _backup_csv($table)
+	{
+		$this->load->dbutil ();
 		$query = NULL;
 		$query = ("SELECT * FROM `$table`");
-		$filename = sprintf('backup-%s-%s.csv',date("Y-m-d-H-i-s"));
-		$path = sprintf("/tmp/");
+		$filename = sprintf ( 's%-backup-%s.csv', $table, date ( "Y-m-d-H-i-s" ) );
+		$path = sprintf ( "/tmp/" );
 		$temp_file = $path . $filename;
 		$delimiter = ",";
 		$newline = "\r\n";
 		$enclosure = '"';
-		$query = $this->db->query($query);
-		$backup =  $this->dbutil->csv_from_result($query, $delimiter, $newline, $enclosure);
-		$this->load->helper('file');
-		write_file($temp_file, $backup);
-		$this->load->helper('download');
-		force_download($filename,$backup);
-		sleep(5);
-		
-		
+		$query = $this->db->query ( $query );
+		$backup = $this->dbutil->csv_from_result ( $query, $delimiter, $newline, $enclosure );
+		$this->load->helper ( 'file' );
+		write_file ( $temp_file, $backup );
+		$this->load->helper ( 'download' );
+		force_download ( $filename, $backup );
 	}
-	
-
 }

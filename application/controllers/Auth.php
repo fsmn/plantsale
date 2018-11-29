16607bcdaa15f220c82fc8aaa4fe01d278e9863a
chrisdart <chrisd@fsmn.org>
2018-11-29 12:51:48 -0600

    Restore normal function after bug fix

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,13 +20,6 @@ class Auth extends CI_Controller {
 
 	// redirect if needed, otherwise display the user list
 	function index() {
-        $this->db->query("CREATE TABLE `login_attempts` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(15) NOT NULL,
-  `login` varchar(100) NOT NULL,
-  `time` int(11) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 		if (! $this->ion_auth->logged_in ()) {
 			// redirect them to the login page
 			redirect ( 'auth/login', 'refresh' );

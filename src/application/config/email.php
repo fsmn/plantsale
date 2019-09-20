7994f38d2e801fa380ca8c4fcf011d67c60f8609
socketwench <tess@deninet.com>
2019-09-19 22:05:06 -0500

    Added template for email.

--- src/application/config/email.php
+++ src/application/config/email.php
@@ -10,17 +10,8 @@ switch($_SERVER['HTTP_HOST']){
 		$config['smtp_user'] = getenv('SMTP_USER');
 		$config['smtp_pass'] = getenv('SMTP_PASS');
 		break;
-	case 'backoffice.t7test.io':
-		$config['smtp_user'] = '{{ TEST_FSMN_SMTP_USER }}';
-		$config['smtp_pass'] = '{{ TEST_FSMN_SMTP_USER }}';
-		break;
-	case 'backoffice.t7stage.io':
-		$config['smtp_user'] = '{{ STAGE_FSMN_SMTP_USER }}';
-		$config['smtp_pass'] = '{{ STAGE_FSMN_SMTP_USER }}';
-		break;
-	case 'backoffice.t7live.io':
-	case 'db.friendsschoolplantsale.com':
-		$config['smtp_user'] = '{{ LIVE_FSMN_SMTP_USER }}';
-		$config['smtp_pass'] = '{{ LIVE_FSMN_SMTP_USER }}';
+	default:
+		$config['smtp_user'] = '{{ backoffice_smtp_user }}';
+		$config['smtp_pass'] = '{{ backoffice_smtp_pass }}';
 		break;
 }

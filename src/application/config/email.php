322705d60764ca3fc7bb8b30961b86c7524987c6
Chris Dart <chrisdart@darter.localdomain>
2019-09-19 19:58:44 -0500

    Update ansible files for flight-deck secrets for test, stage, live

--- src/application/config/email.php
+++ src/application/config/email.php
@@ -5,11 +5,22 @@ $config['smtp_host'] = 'smtp.sendgrid.net';
 $config['smtp_port'] = '587';
 $config['smtp_crypto'] = 'TLS';
 
-if ($_SERVER['HTTP_HOST'] == 'docker.test') {
-	$config['smtp_user'] = getenv('SMTP_USER');
-	$config['smtp_pass'] = getenv('SMTP_PASS');
-}
-else {
-	$config['smtp_user'] = '{{ FSMN_SMTP_USER }}';
-	$config['smtp_pass'] = '{{ FSMN_SMTP_PASS }}';
+switch($_SERVER['HTTP_HOST']){
+	case 'docker.test':
+		$config['smtp_user'] = getenv('SMTP_USER');
+		$config['smtp_pass'] = getenv('SMTP_PASS');
+		break;
+	case 'backoffice.t7test.io':
+		$config['smtp_user'] = '{{ TEST_FSMN_SMTP_USER }}';
+		$config['smtp_pass'] = '{{ TEST_FSMN_SMTP_USER }}';
+		break;
+	case 'backoffice.t7stage.io':
+		$config['smtp_user'] = '{{ STAGE_FSMN_SMTP_USER }}';
+		$config['smtp_pass'] = '{{ STAGE_FSMN_SMTP_USER }}';
+		break;
+	case 'backoffice.t7live.io':
+	case 'db.friendsschoolplantsale.com':
+		$config['smtp_user'] = '{{ LIVE_FSMN_SMTP_USER }}';
+		$config['smtp_pass'] = '{{ LIVE_FSMN_SMTP_USER }}';
+		break;
 }

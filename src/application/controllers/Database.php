8256612c3668df6ccb65a540a02a122f1a036a83
Chris Dart <chris@ten7.com>
2020-11-22 14:34:36 -0600

    Issue #87 create database updater system.

--- src/application/controllers/Database.php
+++ src/application/controllers/Database.php
@@ -1,48 +1,153 @@
 <?php
 
+/**
+ * Class Database
+ */
 class Database extends MY_Controller {
 
+	/**
+	 * Database constructor.
+	 */
 	function __construct() {
 		parent::__construct();
+		$this->load->model('Update_model','updater');
 	}
 
-	function update_database() {
-		$this->db->query('ALTER TABLE `orders` CHANGE `count_midsale` `count_midsale` DECIMAL(10,2) NULL DEFAULT NULL;');
-		$this->db->query('ALTER TABLE `orders` CHANGE `count_presale` `count_presale` DECIMAL(10,2) NULL DEFAULT NULL;');
-		$this->db->query('ALTER TABLE `orders` CHANGE `received_midsale` `received_midsale` DECIMAL(10,2) NULL DEFAULT NULL;');
-		$this->db->query('ALTER TABLE `orders` CHANGE `received_presale` `received_presale` DECIMAL(10,2) NULL DEFAULT NULL;');
-		$this->db->query('ALTER TABLE `orders` CHANGE `count_dead` `count_dead` DECIMAL(10,2) NULL DEFAULT NULL;');
 
-		die();
-		$fields = [
-			'count_wednesday',
-			'count_thursday',
-			'count_friday',
-			'count_saturday',
-			'received_wednesday',
-			'received_thursday',
-			'received_friday',
-			'received_saturday',
-		];
-		$previous_field = 'received_presale';
-		$success = [];
-		$failure = [];
-		foreach ($fields as $field) {
-			try {
-				$this->db->query('ALTER TABLE `orders` ADD `' . $field . '` DECIMAL(10,2) 0.00 AFTER `' . $previous_field . '`;');
-				$success[] = $field;
-
-
-			} catch (Exception $exception) {
-				$failure[] = $field;
+	/**
+	 *
+	 */
+	function run_updates() {
+		$messages = [];
+		foreach (get_class_methods('Database') as $method) {
+			if ($my_method = strstr($method, 'db_update_')) {
+				$id = (int) filter_var($my_method, FILTER_SANITIZE_NUMBER_INT);
+				$query = $this->$my_method();
+				if ($message = $this->updater->update($id, $query)) {
+					$messages[] = $message;
+				}
 			}
-			$previous_field = $field;
 		}
-		$message = sprintf('Successful updates: %s<br/>Errors: %s', implode(', ', $success), implode(', ', $failure));
-
-		$this->session->set_flashdata("notice", $message);
+		$this->session->set_flashdata('notice', implode('<br/>', $messages));
 		redirect();
+	}
 
+	/**
+	 * @return string
+	 */
+	function db_update_2() {
+		return 'ALTER TABLE `orders` CHANGE `count_midsale` `count_midsale` DECIMAL(10,2) NULL DEFAULT NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_3() {
+		return 'ALTER TABLE `orders` CHANGE `count_presale` `count_presale` DECIMAL(10,2) NULL DEFAULT NULL;';
+
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_4() {
+		return 'ALTER TABLE `orders` CHANGE `received_midsale` `received_midsale` DECIMAL(10,2) NULL DEFAULT NULL;';
+
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_5() {
+		return 'ALTER TABLE `orders` CHANGE `received_presale` `received_presale` DECIMAL(10,2) NULL DEFAULT NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_6() {
+		return 'ALTER TABLE `orders` CHANGE `received_presale` `received_presale` DECIMAL(10,2) NULL DEFAULT NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_7() {
+		return 'ALTER TABLE `orders` CHANGE `received_presale` `received_presale` DECIMAL(10,2) NULL DEFAULT NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_8() {
+		return 'ALTER TABLE `orders` CHANGE `count_dead` `count_dead` DECIMAL(10,2) NULL DEFAULT NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_9() {
+		return 'UPDATE `variety` set new_year = 2021 where new_year = 2020;';
+	}
+
+
+	/**
+	 * @return string
+	 */
+	function db_update_10(){
+		return 'ALTER TABLE `orders` ADD `count_wednesday` DECIMAL(10,2) NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_11(){
+		return 'ALTER TABLE `orders` ADD `count_thursday` DECIMAL(10,2) NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_12(){
+		return 'ALTER TABLE `orders` ADD `count_friday` DECIMAL(10,2) NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_13(){
+		return 'ALTER TABLE `orders` ADD `count_saturday` DECIMAL(10,2) NULL;';
+
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_14(){
+		return 'ALTER TABLE `orders` ADD `received_wednesday` DECIMAL(10,2) NULL;';
+
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_15(){
+		return 'ALTER TABLE `orders` ADD `received_thursday` DECIMAL(10,2) NULL;';
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_16(){
+		return 'ALTER TABLE `orders` ADD `received_friday` DECIMAL(10,2) NULL;';
+
+	}
+
+	/**
+	 * @return string
+	 */
+	function db_update_17(){
+		return 'ALTER TABLE `orders` ADD `received_saturday` DECIMAL(10,2) NULL;';
 	}
 
 }

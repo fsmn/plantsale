4f24dbfe8744292bf4a626983fc87ed41d0a6a37
Chris Dart <chris@ten7.com>
2021-01-01 15:03:10 -0600

    Issue #102 fix export.php to not export if now orders are available

--- src/application/views/order/export.php
+++ src/application/views/order/export.php
@@ -1,48 +1,13 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
-//$filename = "order_export.csv";
-// Define the fields desired for output in this array
-$fields = [
-	'grower_id' => 'Grower ID',
-	'year' => 'Year',
-	'catalog_number' => 'Catalog Number',
-	'new_year' => 'First year at Sale',
-	'name' => 'Common Name',
-	'genus' => 'Genus',
-	'species' => 'Species',
-	'variety' => 'Variety',
-	'category' => 'Category',
-	'subcategory' => 'Subcategory',
-	'pot_size' => 'Pot Size',
-	'count_presale' => 'Presale Order',
-	'count_friday' => 'Friday Order',
-	'count_saturday' => 'Saturday Order',
-	'count_midsale' => 'Midsale Order',
-	'flat_count' => 'Total Flats',
-	'received_presale' => 'Received Presale',
-	'received_friday' => 'Received Friday',
-	'received_saturday' => 'Received Saturday',
-	'received_midsale' => 'Received Midsale',
-	'sellout_friday' => 'Sellout Time Friday',
-	'sellout_saturday' => 'Sellout Time Saturday',
-	'remainder_friday' => 'Remainder Friday',
-	'remainder_saturday' => 'Remainder Saturday',
-	'remainder_sunday' => 'Remainder Sunday',
-	'count_dead' => 'Count of Dead Plants',
-	'flat_size' => 'Flat Size',
-	'flat_cost' => 'Flat Cost',
-	'plant_cost' => 'Plant Cost',
-	'price' => 'Sale Price',
-	'flat_area' => 'Flat Area',
-	'tiers' => 'Tiers',
-	'grower_code' => 'Grower Code',
-	'flat_exclude'=> 'Flat Exclude',
-];
-
-
-if ($export_type == "grower") {
+if(!empty($orders)) {
+	//$filename = "order_export.csv";
+	// Define the fields desired for output in this array
 	$fields = [
+		'grower_id' => 'Grower ID',
+		'year' => 'Year',
+		'catalog_number' => 'Catalog Number',
+		'new_year' => 'First year at Sale',
 		'name' => 'Common Name',
 		'genus' => 'Genus',
 		'species' => 'Species',
@@ -50,42 +15,78 @@ if ($export_type == "grower") {
 		'category' => 'Category',
 		'subcategory' => 'Subcategory',
 		'pot_size' => 'Pot Size',
-		'count_presale' => 'Wednesday',
-		'count_midsale' => 'Saturday',
+		'count_presale' => 'Presale Order',
+		'count_friday' => 'Friday Order',
+		'count_saturday' => 'Saturday Order',
+		'count_midsale' => 'Midsale Order',
 		'flat_count' => 'Total Flats',
+		'received_presale' => 'Received Presale',
+		'received_friday' => 'Received Friday',
+		'received_saturday' => 'Received Saturday',
+		'received_midsale' => 'Received Midsale',
+		'sellout_friday' => 'Sellout Time Friday',
+		'sellout_saturday' => 'Sellout Time Saturday',
+		'remainder_friday' => 'Remainder Friday',
+		'remainder_saturday' => 'Remainder Saturday',
+		'remainder_sunday' => 'Remainder Sunday',
+		'count_dead' => 'Count of Dead Plants',
 		'flat_size' => 'Flat Size',
 		'flat_cost' => 'Flat Cost',
 		'plant_cost' => 'Plant Cost',
 		'price' => 'Sale Price',
+		'flat_area' => 'Flat Area',
+		'tiers' => 'Tiers',
 		'grower_code' => 'Grower Code',
+		'flat_exclude' => 'Flat Exclude',
 	];
-}
-$header_values = array_keys($fields);
 
-$output = [
-	implode(",", $header_values),
-];
-foreach ($orders as $order) {
-	$current_year = $order->year;
-	foreach (array_keys($fields) as $key) {
-		if ($key == "new_year") {
-			if ($order->$key == $current_year) {
-				$line[] = "New";
+
+	if ($export_type == "grower") {
+		$fields = [
+			'name' => 'Common Name',
+			'genus' => 'Genus',
+			'species' => 'Species',
+			'variety' => 'Variety',
+			'category' => 'Category',
+			'subcategory' => 'Subcategory',
+			'pot_size' => 'Pot Size',
+			'count_presale' => 'Wednesday',
+			'count_midsale' => 'Saturday',
+			'flat_count' => 'Total Flats',
+			'flat_size' => 'Flat Size',
+			'flat_cost' => 'Flat Cost',
+			'plant_cost' => 'Plant Cost',
+			'price' => 'Sale Price',
+			'grower_code' => 'Grower Code',
+		];
+	}
+	$header_values = array_keys($fields);
+
+	$output = [
+		implode(",", $header_values),
+	];
+	foreach ($orders as $order) {
+		$current_year = $order->year;
+		foreach (array_keys($fields) as $key) {
+			if ($key == "new_year") {
+				if ($order->$key == $current_year) {
+					$line[] = "New";
+				}
+				else {
+					$line[] = "";
+				}
 			}
 			else {
-				$line[] = "";
+				$line[] = $order->$key;
 			}
 		}
-		else {
-			$line[] = $order->$key;
-		}
+		$output[] = "\"" . implode("\",\"", $line) . "\"";
+		$line = NULL;
 	}
-	$output[] = "\"" . implode("\",\"", $line) . "\"";
-	$line = NULL;
-}
 
-$data = implode("\n", $output);
-if(empty($filename)){
-	$filename = 'order_export.csv';
+	$data = implode("\n", $output);
+	if (empty($filename)) {
+		$filename = 'order_export.csv';
+	}
+	force_download($filename, $data);
 }
-force_download($filename, $data);

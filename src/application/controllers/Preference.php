4b477d1af4da50e3f921bd1b9b07a2354dbb0750
Chris Dart <chris@ten7.com>
2021-02-07 18:55:41 -0600

    Issue #114 update user editing and messaging to be more clear and properly restricted.

--- src/application/controllers/Preference.php
+++ src/application/controllers/Preference.php
@@ -10,24 +10,40 @@ class Preference extends MY_Controller{
 		$this->load->model('Preferences_model','preferences');
 		$this->load->model('User_preferences_model','user_preferences');
 	}
+	function index(){
+		redirect('preference/edit/' . $this->ion_auth->get_user_id());
+	}
 
 	function edit($user_id){
-		$data = [
-			'user_id' => $user_id,
-			'action' => 'update',
-			'preferences' => $this->user_preferences->get_all($user_id),
-			'title' => 'Edit Preferences',
-			'target' => 'preference/edit',
-		];
-		$this->load->view('page/index', $data);
+		if($this->ion_auth->is_admin() || $user_id == $this->ion_auth->get_user_id()) {
+			$data = [
+				'user_id' => $user_id,
+				'action' => 'update',
+				'preferences' => $this->user_preferences->get_all($user_id),
+				'title' => 'Edit Preferences',
+				'target' => 'preference/edit',
+			];
+			$this->load->view('page/index', $data);
+		}
+		else {
+			$this->session->set_flashdata('alert','You can only edit your own user preferences');
+			redirect('/');
+		}
 	}
 
 	function update(){
+
 		$user_id = $this->input->post('user_id');
-		$preference_list = $this->user_preferences->get_all($user_id);
-		foreach($preference_list as $preference){
+		if($this->ion_auth->is_admin() || $user_id == $this->ion_auth->get_user_id()) {
+			$preference_list = $this->user_preferences->get_all($user_id);
+			foreach ($preference_list as $preference) {
 				$this->user_preferences->update($user_id, $preference->id, $this->input->post($preference->id));
+			}
+			redirect('preference/edit/' . $user_id);
 		}
-		redirect('preference/edit/' . $user_id);
+		else {
+			$this->session->set_flashdata('alert','You can only edit your own user preferences');
+			redirect('/');
+		}
 	}
 }
--- src/application/views/page/messages.php
+++ src/application/views/page/messages.php
@@ -2,11 +2,11 @@
 $has_message = FALSE;
  if($this->session->flashdata("notice")):?>
 <div id="notice" class="message notice" data-clipboard-target="#notice">
-	<h2>NOTICE</h2><?php echo $this->session->flashdata('notice');?></div>
+	<h2>NOTICE</h2><div><?php echo $this->session->flashdata('notice');?></div></div>
 <?php $has_message = TRUE;?>
 <?php endif;?>
 <?php if($this->session->flashdata("alert")):?>
-	<div id="alert" class="message alert" data-clipboard-target="#alert"><h2>ALERT</h2><?php echo $this->session->flashdata('alert');?></div>
+	<div id="alert" class="message alert" data-clipboard-target="#alert"><h2>ALERT</h2><div><?php echo $this->session->flashdata('alert');?></div></div>
 <?php $has_message = TRUE;?>
 
 <?php endif;?>

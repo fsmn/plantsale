e16505ff3b34a3a935634ba8a7af731adc1604c1
lnewmanheggie <lnewmanheggie@gmail.com>
2021-03-28 16:33:22 -0500

    Issue #93 format sort.php

--- src/application/views/order/sort.php
+++ src/application/views/order/sort.php
@@ -2,53 +2,51 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 $sorting = array(
-        NULL => NULL,
-        "catalog_number" => "Catalog Number",
-        "category" => "Category",
-        "subcategory" => "Subcategory",
-		"name"=>"Common Name",
-        "genus" => "Genus",
-        "species" => "Species",
-        "variety" => "Variety",
-        "grower_code" => "Vendor Code",
-        "count_presale" => "Presale Count",
-        "count_midsale" => "Midsale Count",
-        "pot_size" => "Pot Size",
-		"grower_id"=>"Grower ID",
-		
-        "price"=>"Sale Price",
-		"year"=>"Year",
+    NULL => NULL,
+    'catalog_number' => 'Catalog Number',
+    'category' => 'Category',
+    'subcategory' => 'Subcategory',
+    'name' => 'Common Name',
+    'genus' => 'Genus',
+    'species' => 'Species',
+    'variety' => 'Variety',
+    'grower_code' => 'Vendor Code',
+    'count_presale' => 'Presale Count',
+    'count_midsale' => 'Midsale Count',
+    'pot_size' => 'Pot Size',
+    'grower_id' => 'Grower ID',
+    'price' => 'Sale Price',
+    'year' => 'Year',
 );
+
 $direction = array(
-        "ASC" => "Ascending",
-        "DESC" => "Descending"
+    'ASC' => 'Ascending',
+    'DESC' => 'Descending'
 );
 
 $button = create_button(array(
-        "selection" => "any",
-        "text" => "Add Sort Option",
-        "type" => "span",
-        "class" => "button add-order-sort small"
+    'selection' => 'any',
+    'text' => 'Add Sort Option',
+    'type' => 'span',
+    'class' => 'button add-order-sort small'
 ));
-$saved_sort = cookie("sorting_fields");
+$saved_sort = cookie('sorting_fields');
 if ($saved_sort && $basic_sort == FALSE) {
     $saved_sort = unserialize($saved_sort);
-    $saved_direction = unserialize(cookie("sorting_direction"));
-    for ($i = 0; $i < count($saved_sort); $i ++) {
-        $output[] = "<p>";
-        $output[] = form_dropdown("sorting[]", $sorting, $saved_sort[$i]);
-        $output[] = form_dropdown("direction[]", $direction, $saved_direction[$i]);
+    $saved_direction = unserialize(cookie('sorting_direction'));
+    for ($i = 0; $i < count($saved_sort); $i++) {
+        $output[] = '<p>';
+        $output[] = form_dropdown('sorting[]', $sorting, $saved_sort[$i]);
+        $output[] = form_dropdown('direction[]', $direction, $saved_direction[$i]);
         $output[] = $button;
-        $output[] = "</p>";
+        $output[] = '</p>';
     }
 } else {
-    $output[] = "<p>";
-    $output[] = form_dropdown("sorting[]", $sorting, "genus");
-    $output[] = form_dropdown("direction[]", $direction, "ASC");
+    $output[] = '<p>';
+    $output[] = form_dropdown('sorting[]', $sorting, 'genus');
+    $output[] = form_dropdown('direction[]', $direction, 'ASC');
     $output[] = $button;
-    $output[] = "</p>";
+    $output[] = '</p>';
 }
 
-echo implode("\r", $output);
-
-
+echo implode('\r', $output);

a904229da20e56ff2c171692830c7a858ce0085f
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-19 11:51:18 -0500

    reformatted tracking.php

--- src/application/views/order/tracking.php
+++ src/application/views/order/tracking.php
@@ -15,35 +15,40 @@ if ($orders) :
 	<table class="list tracking small">
 		<thead>
 			<tr>
+				<th class="inventory-tracking-title"><?php print $options['category']; ?></th>
+			</tr>
+			<tr>
 				<th>Cat&#35;</th>
-				<th>Ord'd</th>
-				<th>Per-Flat</th>
-				<th>Rec'd</th>
-				<th class="big">Fri</th>
-				<th class="big">Sat</th>
+				<th>Grower</th>
+				<th>Price</th>
+				<th>Genus</th>
 				<th>Common</th>
-				<th>Latin</th>
 				<th>Variety</th>
 				<th>Pot Size</th>
-				<th>Grower</th>
-				<th>Category</th>
+				<th>Wed<br />Rec'd</th>
+				<th>Fri<br />Rem</th>
+				<th>Sat<br />Rec'd</th>
+				<th>Sat<br />Rem</th>
+				<th>Sun<br />Rem</th>
+				<th>Sun<br />Dead</th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php foreach ($orders as $order) : ?>
 				<tr id="order_<?php print $order->id; ?>">
 					<td><?php print $order->catalog_number; ?></td>
-					<td><?php print $order->count_presale; ?></td>
-					<td><?php print $order->flat_size; ?></td>
-					<td><?php print $order->received_presale; ?></td>
-					<td class="big"></td>
-					<td class="big"></td>
+					<td><?php print $order->grower_id; ?></td>
+					<td><?php print $order->price; ?></td>
+					<td><?php print $order->genus; ?></td>
 					<td class="no-wrap"><?php print $order->name; ?></td>
-					<td class="no-wrap"><?php print format_latin_name($order); ?></td>
 					<td class="no-wrap"><?php print $order->variety; ?></td>
 					<td class="no-wrap"><?php print $order->pot_size; ?></td>
-					<td><?php print $order->grower_id; ?></td>
-					<td><?php print $order->category; ?></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>

e9af6524470332f330f901686faf9fef17974ef1
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-22 10:59:55 -0500

    Issue #136 refactored Controllers/Help.php

--- src/application/controllers/Help.php
+++ src/application/controllers/Help.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Help extends MY_Controller
 {
@@ -6,7 +6,7 @@ class Help extends MY_Controller
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model("help_model");
+		$this->load->model('help_model');
 	}
 
 	/**
@@ -14,9 +14,8 @@ class Help extends MY_Controller
 	 */
 	function get()
 	{
-		$topic = $this->input->get("topic");
-		$subtopic = $this->input->get("subtopic");
-		echo $this->help_model->get($topic, $subtopic);
+		$topic = $this->input->get('topic');
+		$subtopic = $this->input->get('subtopic');
+		print $this->help_model->get($topic, $subtopic);
 	}
-
 }

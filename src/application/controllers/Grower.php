181a2887e42227ad9f8b264d60842456feb68cf5
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-22 11:02:33 -0500

    Issue #136 refactored Controllers/Grower.php

--- src/application/controllers/Grower.php
+++ src/application/controllers/Grower.php
@@ -1,97 +1,102 @@
 <?php
-defined('BASEPATH') or exit ('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Grower extends MY_Controller {
+class Grower extends MY_Controller
+{
 
-	function __construct() {
+	function __construct()
+	{
 		parent::__construct();
 		if (IS_INVENTORY) {
-			redirect("inventory");
+			redirect('inventory');
 		}
-		$this->load->model("grower_model", "grower");
-		$this->load->model("contact_model", "contact");
+		$this->load->model('grower_model', 'grower');
+		$this->load->model('contact_model', 'contact');
 	}
 
-	function view($id) {
+	function view($id)
+	{
 		if ($grower = $this->grower->get($id)) {
 			$grower->contacts = $this->contact->get_for_grower($id);
-			$this->load->model("user_model", "user");
+			$this->load->model('user_model', 'user');
 			$users = $this->user->get_user_pairs();
-			$data ["users"] = get_keyed_pairs($users, [
-				"id",
-				"name",
+			$data['users'] = get_keyed_pairs($users, [
+				'id',
+				'name',
 			], TRUE);
-			$data ["grower"] = $grower;
-			$data ["target"] = "grower/view";
-			$data ["title"] = sprintf("Viewing Details for %s", $id);
-			$this->load->view("page/index", $data);
-		}
-		else {
-			show_error("The grower with ID $id could not be found. Press the back arrow, and notify the database administrator if you believe this error is a mistake.");
+			$data['grower'] = $grower;
+			$data['target'] = 'grower/view';
+			$data['title'] = sprintf('Viewing Details for %s', $id);
+			$this->load->view('page/index', $data);
+		} else {
+			show_error('The grower with ID ' . $id . ' could not be found. Press the back arrow, and notify the database administrator if you believe this error is a mistake.');
 		}
 	}
 
-	function is_unique($id) {
+	function is_unique($id)
+	{
 		$output = TRUE;
 		if ($this->grower->is_unique($id) == 1) {
 			$output = FALSE;
 		}
-		echo $output;
+		print $output;
 		return $output;
 	}
 
-	function show_orphans() {
+	function show_orphans()
+	{
 		$orphans = $this->grower->get_orphans();
-		$data ["title"] = "Orphan Grower Records";
-		$data ["target"] = "grower/orphans";
-		if (!empty ($orphans)) {
-			$data ["message"] = "The Following Orphan Records were Found";
-			$data ["orphans"] = $orphans;
+		$data['title'] = 'Orphan Grower Records';
+		$data['target'] = 'grower/orphans';
+		if (!empty($orphans)) {
+			$data['message'] = 'The Following Orphan Records were Found';
+			$data['orphans'] = $orphans;
+		} else {
+			$data['orphans'] = NULL;
+			$data['message'] = 'No Orphan Growers Were Found.';
 		}
-		else {
-			$data ["orphans"] = NULL;
-			$data ["message"] = "No Orphan Growers Were Found.";
-		}
-		$this->load->view("page/index", $data);
+		$this->load->view('page/index', $data);
 	}
 
-	function create($id = NULL) {
-		$data ["grower"] = NULL;
+	function create($id = NULL)
+	{
+		$data['grower'] = NULL;
 
 		if ($id) {
-			$data ["grower"] = ( object ) [
-				"id" => strtoupper($id),
+			$data['grower'] = (object) [
+				'id' => strtoupper($id),
 			];
 		}
-		$this->load->model("user_model", "user");
+		$this->load->model('user_model', 'user');
 		$users = $this->user->get_user_pairs();
-		$data ["users"] = get_keyed_pairs($users, [
-			"id",
-			"name",
+		$data['users'] = get_keyed_pairs($users, [
+			'id',
+			'name',
 		], TRUE);
-		$data ["action"] = "insert";
-		$data ["target"] = "grower/edit";
-		$data ["title"] = "Add New Grower";
-		if ($this->input->get("ajax") == 1) {
-			$this->load->view("page/modal", $data);
-		}
-		else {
-			$this->load->view("page/index", $data);
+		$data['action'] = 'insert';
+		$data['target'] = 'grower/edit';
+		$data['title'] = 'Add New Grower';
+		if ($this->input->get('ajax') == 1) {
+			$this->load->view('page/modal', $data);
+		} else {
+			$this->load->view('page/index', $data);
 		}
 	}
 
-	function insert() {
+	function insert()
+	{
 		$this->grower->insert();
-		$id = $this->input->post("id");
-		redirect("grower/view/$id");
+		$id = $this->input->post('id');
+		redirect('grower/view/$id');
 	}
 
-	function update_value() {
-		$id = $this->input->post("id");
-		$value = $this->input->post("value");
-		$field = $this->input->post("field");
+	function update_value()
+	{
+		$id = $this->input->post('id');
+		$value = $this->input->post('value');
+		$field = $this->input->post('field');
 		if (is_array($value)) {
-			$value = implode(",", $value);
+			$value = implode(',', $value);
 		}
 		if (!$value) {
 			$value = 0;
@@ -100,75 +105,72 @@ class Grower extends MY_Controller {
 			$field => $value,
 		];
 		$output = $this->grower->update($id, $values);
-		if ($output == "") {
-			$output = "&nbsp;";
+		if ($output == '') {
+			$output = '&nbsp;';
 		}
-		if ($field == "user_id") {
+		if ($field == 'user_id') {
 			if ($value) {
-				$this->load->model("user_model", "user");
+				$this->load->model('user_model', 'user');
 				$user = $this->user->get_user($value);
-				$output = sprintf("%s %s", $user->first_name, $user->last_name);
-			}
-			else {
-				$output = "&nbsp;";
+				$output = sprintf('%s %s', $user->first_name, $user->last_name);
+			} else {
+				$output = '&nbsp;';
 			}
 		}
 
-		echo $output;
+		print $output;
 	}
 
-	function filter() {
+	function filter()
+	{
 		$data = [
 			'title' => 'Filter Growers by Year',
 			'target' => 'grower/filter',
 		];
 		if ($this->input->get('ajax')) {
 			$this->load->view('page/modal', $data);
-		}
-		else {
+		} else {
 			$this->load->view('page/index', $data);
 		}
 	}
 
-	function totals() {
+	function totals()
+	{
 		$year = $this->input->get('year');
 		if (!$year) {
-			$year = cookie("sale_year");
+			$year = cookie('sale_year');
 		}
 		$data['year'] = $year;
-		$data ["orphan_count"] = count($this->grower->get_orphans());
+		$data['orphan_count'] = count($this->grower->get_orphans());
 		$ids = $this->grower->get_ids($year);
 		$growers = [];
 		$grand_total = 0;
 
 		foreach ($ids as $id) {
 			$my_grower = $this->grower->get_totals($id->id, $year);
-			if ($this->input->get("export")) {
-				$growers [] = $my_grower;
-			}
-			else {
-				$growers [] = $this->load->view("grower/report/row", [
-					"grower" => $my_grower,
+			if ($this->input->get('export')) {
+				$growers[] = $my_grower;
+			} else {
+				$growers[] = $this->load->view('grower/report/row', [
+					'grower' => $my_grower,
 					'year' => $year,
 				], TRUE);
 			}
 			$grand_total += $my_grower->total;
 			// $growers[$id] = $this->grower->get_totals($id->grower_id, $year);
 		}
-		$data ["grand_total"] = $grand_total;
-		$data ["ids"] = $ids;
-		$data ["growers"] = $growers;
-		$data ["year"] = $year;
-		$data ["title"] = "Totals Report by Grower for $year";
+		$data['grand_total'] = $grand_total;
+		$data['ids'] = $ids;
+		$data['growers'] = $growers;
+		$data['year'] = $year;
+		$data['title'] = 'Totals Report by Grower for ' . $year;
 
-		if ($this->input->get("export")) {
-			$this->load->helper("download");
-			$this->load->view("grower/report/export", $data);
-		}
-		else {
-			$data ["target"] = "grower/report/list";
-			$this->load->view("page/index", $data);
+		if ($this->input->get('export')) {
+			$this->load->helper('download');
+			$this->load->view('grower/report/export', $data);
+		} else {
+			$data['target'] = 'grower/report/list';
+			$this->load->view('page/index', $data);
 		}
 	}
-
 }

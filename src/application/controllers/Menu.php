ad9e11aff2822b5cabb83feba98c9146eaf1fb39
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-22 10:42:09 -0500

    Issue #136 refactored Controllers/Menu.php

--- src/application/controllers/Menu.php
+++ src/application/controllers/Menu.php
@@ -1,188 +1,194 @@
 <?php
-defined('BASEPATH') or exit ('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Menu extends MY_Controller {
+class Menu extends MY_Controller
+{
 
-	function __construct() {
+	function __construct()
+	{
 		parent::__construct();
 		if (IS_INVENTORY) {
-			redirect("inventory");
+			redirect('inventory');
 		}
-		$this->load->model("menu_model", "menu");
-		$this->load->model("category_model", "category");
-		$this->load->model("subcategory_model", "subcategory");
+		$this->load->model('menu_model', 'menu');
+		$this->load->model('category_model', 'category');
+		$this->load->model('subcategory_model', 'subcategory');
 	}
 
-	function show_all($category = FALSE) {
+	function show_all($category = FALSE)
+	{
 		if (IS_ADMIN) {
-			$data ["items"] = $this->menu->get_all($category);
-			$data ["categories"] = $this->menu->get_categories(FALSE);
+			$data['items'] = $this->menu->get_all($category);
+			$data['categories'] = $this->menu->get_categories(FALSE);
 
 			if ($category) {
-				$data ["title"] = "Showing Menu Items for $category";
+				$data['title'] = 'Showing Menu Items for ' . $category;
+			} else {
+				$data['title'] = 'Showing All Menu Items';
 			}
-			else {
-				$data ["title"] = "Showing All Menu Items";
-			}
+		} else {
+			$data['title'] = 'You are not authorized to view this page.';
+			$data['items'] = NULL;
+			$this->session->set_flashdata('notice', 'You are not authorized to do edit menu items');
 		}
-		else {
-			$data ["title"] = "You are not authorized to view this page.";
-			$data ["items"] = NULL;
-			$this->session->set_flashdata("notice", "You are not authorized to do edit menu items");
-		}
-		$data ["target"] = "menu/list";
+		$data['target'] = 'menu/list';
 
-		$this->load->view("page/index", $data);
+		$this->load->view('page/index', $data);
 	}
 
-	function show_categories() {
-		$data ["categories"] = $this->menu->get_categories(FALSE);
-		$data ["target"] = "menu/categories";
+	function show_categories()
+	{
+		$data['categories'] = $this->menu->get_categories(FALSE);
+		$data['target'] = 'menu/categories';
 	}
 
-	function create() {
+	function create()
+	{
 		if (IS_ADMIN) {
-			$data ["title"] = "Editing a Menu Item";
-			$data ["target"] = "menu/edit";
-			$data ["ajax"] = FALSE;
-			$data ["action"] = "insert";
-			$data ["item"] = NULL;
-			$data ["categories"] = get_keyed_pairs($this->menu->get_categories(FALSE), [
-				"category",
-				"category",
+			$data['title'] = 'Editing a Menu Item';
+			$data['target'] = "menu/edit";
+			$data['ajax'] = FALSE;
+			$data['action'] = 'insert';
+			$data['item'] = NULL;
+			$data['categories'] = get_keyed_pairs($this->menu->get_categories(FALSE), [
+				'category',
+				'category',
 			]);
-			$page = "page/index";
-			if ($this->input->get("ajax")) {
-				$data ["ajax"] = TRUE;
-				$page = $data ["target"];
+			$page = 'page/index';
+			if ($this->input->get('ajax')) {
+				$data['ajax'] = TRUE;
+				$page = $data['target'];
 			}
 			$this->load->view($page, $data);
 		}
 	}
 
-	function edit($id = NULL) {
+	function edit($id = NULL)
+	{
 		if ($id) {
-			$data ["title"] = "Editing a Menu Item";
-			$data ["target"] = "menu/edit";
-			$data ["ajax"] = FALSE;
-			$data ["categories"] = get_keyed_pairs($this->menu->get_categories(FALSE), [
-				"category",
-				"category",
+			$data['title'] = 'Editing a Menu Item';
+			$data['target'] = 'menu/edit';
+			$data['ajax'] = FALSE;
+			$data['categories'] = get_keyed_pairs($this->menu->get_categories(FALSE), [
+				'category',
+				'category',
 			]);
 
-			$page = "page/index";
-			if ($this->input->get("ajax")) {
-				$data ["ajax"] = TRUE;
+			$page = 'page/index';
+			if ($this->input->get('ajax')) {
+				$data['ajax'] = TRUE;
 
-				$page = $data ["target"];
+				$page = $data['target'];
 			}
 
 			if (IS_ADMIN) {
-				$data ["action"] = "update";
-				$data ["item"] = $this->menu->get($id);
-			}
-			else {
-				$data ["item"] = NULL;
-				$data ["title"] = "No Access";
-				$data ["action"] = FALSE;
+				$data['action'] = 'update';
+				$data['item'] = $this->menu->get($id);
+			} else {
+				$data['item'] = NULL;
+				$data['title'] = 'No Access';
+				$data['action'] = FALSE;
 			}
 			$this->load->view($page, $data);
 		}
 	}
 
-	function insert() {
+	function insert()
+	{
 		if (IS_ADMIN) {
 			$item = $this->menu->insert();
-			$this->session->set_flashdata("notice", "The item was successfully added");
+			$this->session->set_flashdata('notice', 'The item was successfully added');
 			redirect("menu/show_all/$item->category");
 		}
 	}
 
-	function update() {
+	function update()
+	{
 		if (IS_ADMIN) {
-			if ($id = $this->input->post("id")) {
+			if ($id = $this->input->post('id')) {
 				$item = $this->menu->update($id);
-				$this->session->set_flashdata("notice", "The item was successfully updated");
+				$this->session->set_flashdata('notice', 'The item was successfully updated');
 
-				redirect("menu/show_all/$item->category");
+				redirect('menu/show_all/' . $item->category);
 			}
 		}
 	}
 
-	function edit_value() {
-		$data ["name"] = $this->input->get("field");
+	function edit_value()
+	{
+		$data['name'] = $this->input->get('field');
 
-		$value = $this->input->get("value");
-		if ($value != "&nbsp;") {
-			$data ["value"] = $value;
-		}
-		else {
-			$data ['value'] = "";
+		$value = $this->input->get('value');
+		if ($value != '&nbsp;') {
+			$data['value'] = $value;
+		} else {
+			$data['value'] = '';
 		}
 		if (is_array($value)) {
-			$data ["value"] = implode(",", $value);
+			$data['value'] = implode(',', $value);
 		}
-		$data ["id"] = $this->input->get("id");
-		$data ["size"] = strlen($data ["value"]) + 5;
-		$data ["type"] = $this->input->get("type");
-		$data ["category"] = $this->input->get("category");
+		$data['id'] = $this->input->get('id');
+		$data["size"] = strlen($data['value']) + 5;
+		$data['type'] = $this->input->get('type');
+		$data['category'] = $this->input->get('category');
 
-		switch ($data ["type"]) {
-			case "dropdown" :
-				$output = $this->_get_dropdown($data ["category"], $data ["value"], $data ["name"]);
+		switch ($data['type']) {
+			case 'dropdown':
+				$output = $this->_get_dropdown($data['category'], $data['value'], $data['name']);
 				break;
-			case "user-dropdown":
-				$output = $this->_get_user_dropdown($data["category"], $data["value"], $data["name"]);
+			case 'user-dropdown':
+				$output = $this->_get_user_dropdown($data['category'], $data['value'], $data['name']);
 				break;
-			case "category-dropdown" :
-				$this->load->model("common_model", "common");
-				$common = $this->common->get($data ["id"]);
-				$output = $this->_get_dropdown($data ["category"], $common->category_id, $data ["name"]);
+			case 'category-dropdown':
+				$this->load->model('common_model', 'common');
+				$common = $this->common->get($data['id']);
+				$output = $this->_get_dropdown($data['category'], $common->category_id, $data['name']);
 				break;
-			case "subcategory-dropdown" :
-				$this->load->model("common_model", "common");
-				$common = $this->common->get($data ["id"]);
-				$output = $this->_get_dropdown($data ["category"], $common->subcategory_id, $data ["name"], $common->category_id);
+			case 'subcategory-dropdown':
+				$this->load->model('common_model', 'common');
+				$common = $this->common->get($data['id']);
+				$output = $this->_get_dropdown($data['category'], $common->subcategory_id, $data['name'], $common->category_id);
 				break;
-			case "pot-size":
-				$this->load->model("order_model", "orders");
+			case 'pot-size':
+				$this->load->model('order_model', 'orders');
 				$pot_sizes = get_keyed_pairs($this->orders->get_pot_sizes(), [
-					"pot_size",
-					"pot_size",
+					'pot_size',
+					'pot_size',
 				], FALSE, TRUE);
 				$output = form_dropdown('pot_size', $pot_sizes, urlencode($data['value']));
 				break;
-			case "multiselect" :
-				$output = $this->_get_multiselect($data ["category"], $data ["value"], $data ["name"]);
+			case 'multiselect':
+				$output = $this->_get_multiselect($data['category'], $data['value'], $data['name']);
 				break;
-			case "textarea" :
-				$output = form_textarea($data, $data ["value"]);
+			case 'textarea':
+				$output = form_textarea($data, $data['value']);
 				break;
-			case "autocomplete" :
-				$data ["type"] = "text";
-				$output = form_input($data, $data ["value"], "class='autocomplete'");
+			case 'autocomplete':
+				$data['type'] = 'text';
+				$output = form_input($data, $data['value'], 'class="autocomplete"');
 				break;
-			case "time" :
-			case "email" :
-				$output = sprintf("<input type'%s' name='%s' id='%s' value='%s' size='%s'", $data ['type'], $data ['name'], $data ['id'], $data ['value'], $data ['size']);
+			case 'time':
+			case 'email':
+				$output = sprintf('<input type"%s" name="%s" id="%s" value="%s" size="%s"', $data['type'], $data['name'], $data['id'], $data['value'], $data['size']);
 				break;
-			default :
+			default:
 				$output = form_input($data);
 		}
 
-		echo $output;
+		print $output;
 	}
 
 	/**
 	 * AJAX function to create quick-edit dropdown <select> fields (for use with
 	 * the field editing AJAX functions in general.js
 	 */
-	function get_dropdown() {
-		$category = $this->input->get("category");
-		$value = $this->input->get("value");
-		$field = $this->input->get("field");
-		$parent_id = $this->input->get("parent");
-		echo $this->_get_dropdown($category, $value, $field, $parent_id);
+	function get_dropdown()
+	{
+		$category = $this->input->get('category');
+		$value = $this->input->get('value');
+		$field = $this->input->get('field');
+		$parent_id = $this->input->get('parent');
+		print $this->_get_dropdown($category, $value, $field, $parent_id);
 	}
 
 	/**
@@ -190,137 +196,141 @@ class Menu extends MY_Controller {
 	 * multiple="multiple"> fields
 	 * (for use with the field editing AJAX functions in general.js
 	 */
-	function get_multiselect() {
-		$category = $this->input->get("category");
-		$value = explode(",", $this->input->get("value"));
-		$field = $this->input->get("field");
+	function get_multiselect()
+	{
+		$category = $this->input->get('category');
+		$value = explode(",", $this->input->get('value'));
+		$field = $this->input->get('field');
 		$categories = $this->menu->get_pairs($category, [
-			"field" => "value",
-			"direction" => "ASC",
+			'field' => 'value',
+			'direction' => 'ASC',
 		]);
 		$pairs = get_keyed_pairs($categories, [
-			"key",
-			"value",
+			'key',
+			'value',
 		]);
 		$output = [];
-		$output [] = form_multiselect($field, $pairs, $value, "id='$field'");
-		$buttons = implode(" ", $output);
-		echo $buttons . sprintf($field);
+		$output[] = form_multiselect($field, $pairs, $value, "id='$field'");
+		$buttons = implode(' ', $output);
+		print $buttons . sprintf($field);
 	}
 
 	/**
 	 * This function is not currently used because parsing check boxes in AJAX
 	 * is a pain in the ass.
 	 */
-	function get_checkbox() {
-		$category = $this->input->get("category");
-		$value = $this->input->get("value");
-		$field = $this->input->get("field");
+	function get_checkbox()
+	{
+		$category = $this->input->get('category');
+		$value = $this->input->get('value');
+		$field = $this->input->get('field');
 		$categories = $this->menu->get_pairs($category, [
-			"field" => "value",
-			"direction" => "ASC",
+			'field' => 'value',
+			'direction' => 'ASC',
 		]);
 		$pairs = get_keyed_pairs($categories, [
-			"key",
-			"value",
+			'key',
+			'value',
 		]);
 
 		$output = [];
 		for ($i = 0; $i < count($categories); $i++) {
-			$checked = "";
-			$item = $categories [$i];
+			$checked = '';
+			$item = $categories[$i];
 			if ($item->value == $value) {
-				$checked = "checked";
+				$checked = 'checked';
 			}
 
-			$output [] = sprintf("<label for='%s'>%s</label><input type='checkbox' name='%s[$i]' id='%s' value='%s' %s/>", $item->value, $item->value, $field, $field, $item->value, $checked);
+			$output[] = sprintf("<label for='%s'>%s</label><input type='checkbox' name='%s[$i]' id='%s' value='%s' %s/>", $item->value, $item->value, $field, $field, $item->value, $checked);
 		}
-		$buttons = implode(" ", $output);
-		echo $buttons . sprintf("<span class='button save-checkbox' target='%s'>Save</span>", $field);
+		$buttons = implode(' ', $output);
+		print $buttons . sprintf("<span class='button save-checkbox' target='%s'>Save</span>", $field);
 	}
 
-	function get_autocomplete() {
-		$category = $this->input->get("category");
-		$value = $this->input->get("value");
-		$id = $this->input->get("id");
+	function get_autocomplete()
+	{
+		$category = $this->input->get('category');
+		$value = $this->input->get('value');
+		$id = $this->input->get('id');
 		$is_live = FALSE;
-		if ($this->input->get("is_live")) {
-			$is_live = $this->input->get("is_live");
+		if ($this->input->get('is_live')) {
+			$is_live = $this->input->get('is_live');
 		}
 		switch ($category) {
-			case "category" :
+			case 'category':
 				$categories = $this->category->get_pairs();
 				break;
-			case "subcategory" :
-				$category_id = $this->category->get_id($this->input->get("parent"));
+			case 'subcategory':
+				$category_id = $this->category->get_id($this->input->get('parent'));
 				$categories = $this->subcategory->get_pairs($category_id);
 				break;
-			default :
+			default:
 
 				$categories = $this->menu->get_pairs($category, [
-					"field" => "value",
-					"direction" => "ASC",
+					'field' => 'value',
+					'direction' => 'ASC',
 				]);
 		}
 
-		// echo create_autocomplete($categories, $value, $id, $is_live);
-		echo create_list($categories);
-		// echo form_dropdown ( $field, $pairs, $value, "class='save-field'" );
+		// print create_autocomplete($categories, $value, $id, $is_live);
+		print create_list($categories);
+		// print form_dropdown ( $field, $pairs, $value, "class='save-field'" );
 	}
 
-	function _get_dropdown($category, $value, $field, $parent_id = FALSE) {
+	function _get_dropdown($category, $value, $field, $parent_id = FALSE)
+	{
 		switch ($category) {
-			case "category" :
+			case 'category':
 				$categories = $this->category->get_pairs();
 				break;
-			case "subcategory" :
+			case 'subcategory':
 				if ($parent_id) {
 					$categories = $this->subcategory->get_pairs($parent_id);
-				}
-				else {
+				} else {
 					$categories = $this->subcategory->get_pairs();
 				}
 
 				break;
-			default :
+			default:
 
 				$categories = $this->menu->get_pairs($category, [
-					"field" => "value",
-					"direction" => "ASC",
+					'field' => 'value',
+					'direction' => 'ASC',
 				]);
 		}
 
 		$pairs = get_keyed_pairs($categories, [
-			"key",
-			"value",
+			'key',
+			'value',
 		], TRUE);
 		return form_dropdown($field, $pairs, $value, "class='live-field'");
 	}
 
-	function _get_user_dropdown($category, $value, $field) {
-		$this->load->model("user_model", "user");
+	function _get_user_dropdown($category, $value, $field)
+	{
+		$this->load->model('user_model', 'user');
 		$users = $this->user->get_user_pairs();
-		$pairs = get_keyed_pairs($users, ["id", "name"], TRUE);
+		$pairs = get_keyed_pairs($users, ['id', 'name'], TRUE);
 		if ($value) {
 			$value = $this->user->get_by_name($value);
 		}
 		return form_dropdown($field, $pairs, $value, "class='live-field'");
 	}
 
-	function _get_multiselect($category, $value, $field) {
-		$this->load->model("menu_model", "menu");
+	function _get_multiselect($category, $value, $field)
+	{
+		$this->load->model('menu_model', 'menu');
 		$categories = $this->menu->get_pairs($category, [
-			"field" => "value",
-			"direction" => "ASC",
+			'field' => 'value',
+			'direction' => 'ASC',
 		]);
 		$pairs = get_keyed_pairs($categories, [
-			"key",
-			"value",
+			'key',
+			'value',
 		]);
 		$output = [];
-		$output [] = form_multiselect($field, $pairs, explode(",", $value), "id='$field'");
-		$buttons = implode(" ", $output);
-		echo $buttons . sprintf($field);
+		$output[] = form_multiselect($field, $pairs, explode(',', $value), "id='$field'");
+		$buttons = implode(' ', $output);
+		print $buttons . sprintf($field);
 	}
-
 }

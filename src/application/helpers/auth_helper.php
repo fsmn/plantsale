c56fcea29443f48461a21204efeba4c389d36045
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-22 11:11:12 -0500

    Issue #136 refactored helpers/auth_helper.php

--- src/application/helpers/auth_helper.php
+++ src/application/helpers/auth_helper.php
@@ -6,9 +6,9 @@ function set_user_cookies ($cookies)
 {
     foreach ($cookies as $cookie) {
         $data = array();
-        $data["name"] = $cookie->type;
-        $data["value"] = $cookie->value;
-        $data["expire"] = 0;
+        $data['name'] = $cookie->type;
+        $data['value'] = $cookie->value;
+        $data['expire'] = 0;
         set_cookie($data);
     }
 }

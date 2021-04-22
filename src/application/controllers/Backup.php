b2f3357304633fee6c0f7be1a3fe0cbcbda4f9b5
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-22 09:36:57 -0500

    Issue #136 refactored Controllers/Backup.php

--- src/application/controllers/Backup.php
+++ src/application/controllers/Backup.php
@@ -10,9 +10,9 @@ class Backup extends MY_Controller {
 
 	function index() {
 		$this->load->dbutil();
-		$tables = $this->db->query("SHOW TABLES")->result();
-		$data ['target'] = "backup/control";
-		$data ['title'] = "Backup Data Tables";
+		$tables = $this->db->query('SHOW TABLES')->result();
+		$data ['target'] = 'backup/control';
+		$data ['title'] = 'Backup Data Tables';
 		foreach ($tables as $table => $item) {
 			foreach ($item as $string) {
 				$data ['tables'] [] = $string;
@@ -21,14 +21,14 @@ class Backup extends MY_Controller {
 		$this->load->view('page/index', $data);
 	}
 
-	function backup_table($table, $type = "sql") {
-		if ($type == "sql") {
+	function backup_table($table, $type = 'sql') {
+		if ($type == 'sql') {
 			ini_set('memory_limit', '4096M');
 
 			// Load the DB utility class
 			$this->load->dbutil();
-			if ($table == "user_sessions") {
-				$this->db->query("DELETE FROM `user_sessions` WHERE timestamp < UNIX_TIMESTAMP() - 5000");
+			if ($table == 'user_sessions') {
+				$this->db->query('DELETE FROM `user_sessions` WHERE timestamp < UNIX_TIMESTAMP() - 5000');
 			}
 
 			if (!is_array($table)) {
@@ -41,8 +41,8 @@ class Backup extends MY_Controller {
 
 			$backup = $this->dbutil->backup($prefs);
 
-			$filename = sprintf("%s-backup-%s.sql.gz", join("_", $table), date("Y-m-d-H-i-s"));
-			$path = sprintf("/tmp/");
+			$filename = sprintf('%s-backup-%s.sql.gz', join('_', $table), date('Y-m-d-H-i-s'));
+			$path = sprintf('/tmp/');
 			$temp_file = $path . $filename;
 			// Load the file helper and write the file to your server
 			$this->load->helper('file');
@@ -51,15 +51,15 @@ class Backup extends MY_Controller {
 			$this->load->helper('download');
 			force_download($filename, $backup);
 		}
-		elseif ($type == "csv") {
+		elseif ($type == 'csv') {
 			$this->_backup_csv($table);
 		}
-		redirect("/");
+		redirect('/');
 	}
 
 	function full_backup() {
 		ini_set('memory_limit', '4096M');
-		$this->db->query("DELETE FROM `user_sessions` WHERE timestamp < UNIX_TIMESTAMP() - 5000");
+		$this->db->query('DELETE FROM `user_sessions` WHERE timestamp < UNIX_TIMESTAMP() - 5000');
 		$this->db->query('TRUNCATE table `login_attempts`');
 		// Load the DB utility class
 		//ignore certain tables
@@ -68,8 +68,8 @@ class Backup extends MY_Controller {
 		$settings = [];
 		// Backup your entire database and assign it to a variable
 		$backup = $this->dbutil->backup($settings);
-		$filename = sprintf("backup-%s.sql.gz", date("Y-m-d-H-i-s"));
-		$path = sprintf("/tmp/");
+		$filename = sprintf('backup-%s.sql.gz', date('Y-m-d-H-i-s'));
+		$path = sprintf('/tmp/');
 		$temp_file = $path . $filename;
 		// Load the file helper and write the file to your server
 		$this->load->helper('file');
@@ -77,18 +77,18 @@ class Backup extends MY_Controller {
 		// Load the download helper and send the file to your desktop
 		$this->load->helper('download');
 		force_download($filename, $backup);
-		redirect("/");
+		redirect('/');
 	}
 
 	function _backup_csv($table) {
 		$this->load->dbutil();
 		$query = NULL;
-		$query = ("SELECT * FROM `$table`");
-		$filename = sprintf('s%-backup-%s.csv', $table, date("Y-m-d-H-i-s"));
-		$path = sprintf("/tmp/");
+		$query = ('SELECT * FROM `$table`');
+		$filename = sprintf('s%-backup-%s.csv', $table, date('Y-m-d-H-i-s'));
+		$path = sprintf('/tmp/');
 		$temp_file = $path . $filename;
-		$delimiter = ",";
-		$newline = "\r\n";
+		$delimiter = ',';
+		$newline = '\r\n';
 		$enclosure = '"';
 		$query = $this->db->query($query);
 		$backup = $this->dbutil->csv_from_result($query, $delimiter, $newline, $enclosure);
@@ -96,7 +96,7 @@ class Backup extends MY_Controller {
 		write_file($temp_file, $backup);
 		$this->load->helper('download');
 		force_download($filename, $backup);
-		redirect("/");
+		redirect('/');
 	}
 
 	function critical() {

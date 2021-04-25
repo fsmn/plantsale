f10d8bcbc3a6839ccec0fe93cd6513766331f066
Chris Dart <chris@cerebratorium.com>
2021-04-24 20:26:30 -0500

    Issue #96 alter columns and formatting per pat's request

--- src/application/views/order/tracking.php
+++ src/application/views/order/tracking.php
@@ -6,12 +6,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 if ($orders) :
 	?>
 	<!-- order/tracking -->
-	<h3>Inventory Tracking Printable Report</h3>
+	<h3 class="no-print">Inventory Tracking Printable Report</h3>
 
 	<table class="list tracking small">
 		<thead>
 		<tr>
-			<th class="inventory-tracking-title"><?php print $options['category']; ?></th>
+			<th class="inventory-tracking-title" colspan="14"><?php print $options['category']; ?></th>
 		</tr>
 		<tr>
 			<td colspan="14">
@@ -31,29 +31,28 @@ if ($orders) :
 		<tr>
 			<th>Cat&#35;</th>
 			<th>Grower</th>
-			<th>Price</th>
 			<th>Genus</th>
 			<th>Common</th>
 			<th>Variety</th>
 			<th>Pot Size</th>
+			<th>Cat&#35;</th>
 			<th>Wed<br/>Rec'd</th>
 			<th>Fri<br/>Rem</th>
 			<th>Sat<br/>Rec'd</th>
 			<th>Sat<br/>Rem</th>
 			<th>Sun<br/>Rem</th>
-			<th>Sun<br/>Dead</th>
 		</tr>
 		</thead>
 		<tbody>
 		<?php foreach ($orders as $order) : ?>
 			<tr id="order_<?php print $order->id; ?>">
-				<td><?php print $order->catalog_number; ?></td>
+				<td><strong><?php print $order->catalog_number; ?></strong></td>
 				<td><?php print $order->grower_id; ?></td>
-				<td><?php print $order->price; ?></td>
 				<td><?php print $order->genus; ?></td>
 				<td class="no-wrap"><?php print $order->name; ?></td>
 				<td class="no-wrap"><?php print $order->variety; ?></td>
 				<td class="no-wrap"><?php print $order->pot_size; ?></td>
+				<td><strong><?php print $order->catalog_number; ?></strong></td>
 				<td></td>
 				<td></td>
 				<td></td>

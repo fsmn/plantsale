e655754229623305b715a0e4003c0563a3e9f47c
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-25 19:14:43 -0500

    fixed tab out of text area issue so the page does not jump to the bottom in general.js

--- src/js/general.js
+++ src/js/general.js
@@ -180,8 +180,6 @@
 				url: base_url + "menu/edit_value",
 				data: form_data,
 				success: function (data) {
-					// console.log(data);
-					// console.log(my_parent.children());
 					my_parent.children(".edit-field").html(data).removeClass("edit-field").removeClass("field").addClass("live-field").addClass("text");
 					//$("#" + my_parent + " .live-field input").focus();
 				}
@@ -539,6 +537,8 @@ function update_field(me, my_type) {
 		data: form_data,
 		success: function (data) {
 			console.log(data);
+			// redirects focus back on textarea
+			me.focus();
 			if (!is_persistent) {
 				$(my_parent).html(data)
 				$(my_parent).removeClass("live-field text");

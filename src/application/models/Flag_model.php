3b43bc27038a9ae427ec921d22eea8ee8118d4a9
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-26 09:33:57 -0500

    Issue #136 refactored flag_model

--- src/application/models/Flag_model.php
+++ src/application/models/Flag_model.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 class Flag_Model extends MY_Model
 {
@@ -14,11 +14,11 @@ class Flag_Model extends MY_Model
 
 	function prepare_variables()
 	{
-		$variables = array("variety_id","name");
+		$variables = ['variety_id', 'name'];
 
-		for($i = 0; $i < count($variables); $i++){
+		for ($i = 0; $i < count($variables); $i++) {
 			$my_variable = $variables[$i];
-			if($this->input->post($my_variable)){
+			if ($this->input->post($my_variable)) {
 				$this->$my_variable = urldecode($this->input->post($my_variable));
 			}
 		}
@@ -31,13 +31,13 @@ class Flag_Model extends MY_Model
 	function insert()
 	{
 		$this->prepare_variables();
-		$id = $this->_insert("flag");
+		$id = $this->_insert('flag');
 		return $id;
 	}
 
-	function update($id, $values = array())
+	function update($id, $values = [])
 	{
-	    return $this->_update("flag",$id, $values);
+		return $this->_update('flag', $id, $values);
 	}
 
 	/**
@@ -45,38 +45,40 @@ class Flag_Model extends MY_Model
 	 * @param array $ids
 	 * @param string $flag
 	 */
-	function batch_update($variety_ids, $flag){
+	function batch_update($variety_ids, $flag)
+	{
 		$rec_modified = mysql_timestamp();
 		$rec_modifier = $this->ion_auth->get_user_id();
-		$values = array();
+		$values = [];
 
-		foreach($variety_ids as $id){
-			$values[] = sprintf("(%s,'%s','%s','%s')", $id, $flag, $rec_modified, $rec_modifier);
+		foreach ($variety_ids as $id) {
+			$values[] = sprintf('(%s,"%s","%s","%s")', $id, $flag, $rec_modified, $rec_modifier);
 		}
-		$query = sprintf("REPLACE INTO flag (`variety_id`,`name`,`rec_modified`,`rec_modifier`) VALUES%s;", implode(",",$values));
+		$query = sprintf('REPLACE INTO flag (`variety_id`,`name`,`rec_modified`,`rec_modifier`) VALUES%s;', implode(',', $values));
 		$this->db->query($query);
-
 	}
 
 	function delete($id)
 	{
-	    return $this->_delete("flag",$id);
+		return $this->_delete('flag', $id);
 	}
 
-	function get_for_variety($variety_id){
-		$this->db->where("variety_id",$variety_id);
-		$this->db->from("flag");
-		$this->db->select("flag.*,icon.source,icon.thumbnail");
-		$this->db->join("menu","flag.name=menu.key");
-		$this->db->join("icon","menu.id=icon.menu_id");
+	function get_for_variety($variety_id)
+	{
+		$this->db->where('variety_id', $variety_id);
+		$this->db->from('flag');
+		$this->db->select('flag.*,icon.source,icon.thumbnail');
+		$this->db->join('menu', 'flag.name=menu.key');
+		$this->db->join('icon', 'menu.id=icon.menu_id');
 		$output = $this->db->get()->result();
 		return $output;
 	}
 
-	function get_for_web($variety_id){
-	    $this->db->where("variety_id",$variety_id);
-		$this->db->from("flag");
-		$this->db->select("flag.name");
+	function get_for_web($variety_id)
+	{
+		$this->db->where('variety_id', $variety_id);
+		$this->db->from('flag');
+		$this->db->select('flag.name');
 		$output = $this->db->get()->result_array();
 		return $output;
 	}
@@ -86,17 +88,17 @@ class Flag_Model extends MY_Model
 	 * @param unknown $variety_id
 	 * @return array
 	 */
-	function get_missing($variety_id){
+	function get_missing($variety_id)
+	{
 		$current_flags = $this->get_for_variety($variety_id);
-		$flag_list = array();
-		foreach($current_flags as $current_flag){
+		$flag_list = [];
+		foreach ($current_flags as $current_flag) {
 			$flag_list[] = $current_flag->name;
 		}
-		$query = sprintf("SELECT `key`, `value` FROM `menu` WHERE `category` = 'flag' AND `value` not in ('%s')",implode("','", $flag_list));
+		$query = sprintf('SELECT `key`, `value` FROM `menu` WHERE `category` = "flag" AND `value` not in ("%s")', implode('","', $flag_list));
 
 		$output = $this->db->query($query)->result();
 
 		return $output;
 	}
-
 }

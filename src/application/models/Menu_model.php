373d69b3362708143387e93c1b6139d776834760
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-26 09:43:35 -0500

    Issue #136 refactored menu_model

--- src/application/models/Menu_model.php
+++ src/application/models/Menu_model.php
@@ -10,20 +10,20 @@ class Menu_Model extends MY_Model
     var $key;
     var $value;
 
-    function __construct ()
+    function __construct()
     {
         parent::__construct();
     }
 
-    function prepare_variables ()
+    function prepare_variables()
     {
-        $variables = array(
-                "category",
-                "key",
-                "value"
-        );
+        $variables = [
+            'category',
+            'key',
+            'value'
+        ];
 
-        for ($i = 0; $i < count($variables); $i ++) {
+        for ($i = 0; $i < count($variables); $i++) {
             $my_variable = $variables[$i];
             if ($this->input->post($my_variable)) {
                 $this->$my_variable = $this->input->post($my_variable);
@@ -31,94 +31,94 @@ class Menu_Model extends MY_Model
         }
     }
 
-    function insert ()
+    function insert()
     {
         if (IS_ADMIN) {
             $this->prepare_variables();
 
-           return $this->get( $this->_insert("menu"));
+            return $this->get($this->_insert('menu'));
         }
     }
 
-    function update ($id)
+    function update($id)
     {
         if (IS_ADMIN) {
-            $this->db->where("id", $id);
+            $this->db->where('id', $id);
             $this->prepare_variables();
-            $this->db->update("menu", $this);
+            $this->db->update('menu', $this);
             return $this->get($id);
         }
     }
 
-    function get ($id)
+    function get($id)
     {
-        return $this->_get("menu", $id);
+        return $this->_get('menu', $id);
     }
 
-    function get_id ($category, $key, $value)
+    function get_id($category, $key, $value)
     {
-        $this->db->where("category", $category);
-        $this->db->where("key", $key);
-        $this->db->where("value", $value);
-        $this->db->from("menu");
+        $this->db->where('category', $category);
+        $this->db->where('key', $key);
+        $this->db->where('value', $value);
+        $this->db->from('menu');
         $output = $this->db->get()->row();
         return $output;
     }
 
-    function get_categories ($pairs = TRUE)
+    function get_categories($pairs = TRUE)
     {
-        $this->db->from("menu");
-        if($pairs){
-        $this->db->select("`category` as 'key',`category` as 'value'");
-        }else{
-            $this->db->select("category");
+        $this->db->from('menu');
+        if ($pairs) {
+            $this->db->select('`category` as "key",`category` as "value"');
+        } else {
+            $this->db->select('category');
         }
-        $this->db->group_by("category");
-        $this->db->order_by("category", "ASC");
+        $this->db->group_by('category');
+        $this->db->order_by('category', 'ASC');
         $result = $this->db->get()->result();
         return $result;
     }
 
-    function get_all ($category = FALSE)
+    function get_all($category = FALSE)
     {
-        $this->db->from("menu");
-        $this->db->order_by("category", "ASC");
-        $this->db->order_by("value");
+        $this->db->from('menu');
+        $this->db->order_by('category', 'ASC');
+        $this->db->order_by('value');
         if ($category) {
-            $this->db->where("category", $category);
+            $this->db->where('category', $category);
         }
         $result = $this->db->get()->result();
         return $result;
     }
 
-    function get_pairs ($category, $order_by = array())
+    function get_pairs($category, $order_by = [])
     {
-    	
+
         $this->db->where('category', $category);
         $this->db->select(['key', 'value']);
-        $direction = "ASC";
-        $order_field = "key";
-        
+        $direction = 'ASC';
+        $order_field = 'key';
+
         if (!empty($order_by)) {
-            if (array_key_exists("direction", $order_by)) {
+            if (array_key_exists('direction', $order_by)) {
                 $direction = $order_by['direction'];
             }
-            if (array_key_exists("field", $order_by)) {
+            if (array_key_exists('field', $order_by)) {
                 $order_field = $order_by['field'];
             }
         }
-        
+
         $this->db->order_by($order_field, $direction);
         $this->db->from('menu');
         $result = $this->db->get()->result();
-        return $result;        
+        return $result;
     }
 
-    function get_value ($category, $key)
+    function get_value($category, $key)
     {
-        $this->db->from("menu");
-        $this->db->where("category", $category);
-        $this->db->where("key", $key);
+        $this->db->from('menu');
+        $this->db->where('category', $category);
+        $this->db->where('key', $key);
         $value = $this->db->get()->row()->value;
         return $value;
     }

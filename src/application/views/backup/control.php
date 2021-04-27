4c8a9cc92856ed80c760ca977853d9cb627a441a
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-26 19:52:08 -0500

    Issue #136 refactored all files in backup

--- src/application/views/backup/control.php
+++ src/application/views/backup/control.php
@@ -1,16 +1,17 @@
 <?php ?>
 
-    <h2><?php echo $title; ?></h2>
-    <p>Click each button to download the data table. The critical tables for catastrophic recovery are common, contact,
-        flag, grower, orders, variety, and user</p>
-<p>Please note: running the complete backup may log some users out. It will not log out users who are currently making changes to the database. </p>
+<h2><?php print $title; ?></h2>
+<p>Click each button to download the data table. The critical tables for catastrophic recovery are common, contact,
+    flag, grower, orders, variety, and user</p>
+<p>Please note: running the complete backup may log some users out. It will not log out users who are currently making
+    changes to the database. </p>
 
 
 <?php
-$exclude = array(
+$exclude = [
     'common_archive',
-);
-$critical = array(
+];
+$critical = [
     'common',
     'category',
     'flag',
@@ -20,40 +21,43 @@ $critical = array(
     'image',
     'user',
     'subcategory'
-);
+];
 
-$buttons [] = array('selection' => 'backup',
+$buttons[] = [
+    'selection' => 'backup',
     'text' => 'Complete Backup',
-    'class' => array('button', 'btn', 'export'),
+    'class' => ['button', 'btn', 'export'],
     'style' => 'default',
     'href' => site_url('backup/full_backup'),
     'title' => 'Download a complete backup of every table in the database. This may force some users to log out.',
-);
+];
 
-$buttons[] = array('selection' => 'all',
+$buttons[] = [
+    'selection' => 'all',
     'text' => 'Download All Critical Databases',
-    'class' => array('button', 'btn', 'export'),
+    'class' => ['button', 'btn', 'export'],
     'style' => 'default',
     'href' => site_url('backup/critical/'),
     'title' => 'Create a download of all critical tables',
-);
+];
 foreach ($tables as $table) :
     if (!in_array($table, $exclude)) {
-        $class = array(
+        $class = [
             'button',
             'btn',
             'export'
-        );
+        ];
         $title = in_array($table, $critical) ? '(CRITICAL)' : '';
 
-        $buttons[] = array('selection' => 'all',
+        $buttons[] = [
+            'selection' => 'all',
             'text' => sprintf('Download %s %s', $table, $title),
             'class' => $class,
             'style' => 'default',
-            'href' => site_url("backup/backup_table/$table"),
-            'title' => "Create a downloadable backup of the $table table");
-
+            'href' => site_url('backup/backup_table/' . $table),
+            'title' => 'Create a downloadable backup of the ' . $table . ' table'
+        ];
     }
 endforeach;
 
-echo create_button_bar($buttons, array('class' => 'vertical-list'));
+print create_button_bar($buttons, ['class' => 'vertical-list']);

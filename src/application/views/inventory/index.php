60b9e1bb9d42b92e9103ba788e8232bb3ec064d3
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 10:29:46 -0500

    Issue #136 refactored all files in views/inventory

--- src/application/views/inventory/index.php
+++ src/application/views/inventory/index.php
@@ -1,27 +1,30 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <html>
+
 <head>
-   <meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title><?php echo $title;?></title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" media="screen">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title><?php print $title; ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" media="screen">
 
-<!-- Bootstrap theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" media="screen">
-<link rel="stylesheet" href="<?php echo base_url("css/inventory.css?") . date("U");?>" media="screen">
+    <!-- Bootstrap theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" media="screen">
+    <link rel="stylesheet" href="<?php print base_url('css/inventory.css?') . date('U'); ?>" media="screen">
 
 </head>
+
 <body>
-<div id="page">
-<a href="<?php echo site_url("auth/logout");?>" class="btn btn-link">Log Out</a>
-<?php if($this->session->flashdata("warning")):?>
-<div class="alert alert-warning" role="alert"><?php echo $this->session->flashdata("warning");?></div>
-<?php endif; ?>
-<h3 class="title"><?php echo $title;?></h3>
-<?php $this->load->view($target);?>
-</div>
+    <div id="page">
+        <a href="<?php print site_url('auth/logout'); ?>" class="btn btn-link">Log Out</a>
+        <?php if ($this->session->flashdata('warning')) : ?>
+            <div class="alert alert-warning" role="alert"><?php print $this->session->flashdata('warning'); ?></div>
+        <?php endif; ?>
+        <h3 class="title"><?php print $title; ?></h3>
+        <?php $this->load->view($target); ?>
+    </div>
 </body>
+
 </html>
--- src/application/views/inventory/verify.php
+++ src/application/views/inventory/verify.php
@@ -2,15 +2,16 @@
 
 ?>
 <h4>
-<?php printf("Is %s: %s %s (%s) Correct?",$item->catalog_number, $item->name,$item->variety, format_latin_name($item));?></h4>
+    <?php printf('Is %s: %s %s (%s) Correct?', $item->catalog_number, $item->name, $item->variety, format_latin_name($item)); ?>
+</h4>
 
-<img class="small" src="<?php echo base_url($item->image_path);?>"/>
+<img class="small" src="<?php print base_url($item->image_path); ?>" />
 <p>
-<span style="display: inline-block">
-<a href="<?php echo site_url("inventory/verify/$item->catalog_number");?>" class="btn btn-success btn-lg" style="padding-left: 3em;padding-right:3em;">Yes</a>
-</span>
-&nbsp;
-<span style="display: inline-block; float: right;">
-<a href="<?php echo site_url("inventory/index");?>" class="btn btn-danger btn-lg" style="padding-left:3em;padding-right:3em;">No</a>
-</span>
+    <span style="display: inline-block">
+        <a href="<?php print site_url('inventory/verify/' . $item->catalog_number); ?>" class="btn btn-success btn-lg" style="padding-left: 3em;padding-right:3em;">Yes</a>
+    </span>
+    &nbsp;
+    <span style="display: inline-block; float: right;">
+        <a href="<?php print site_url('inventory/index'); ?>" class="btn btn-danger btn-lg" style="padding-left:3em;padding-right:3em;">No</a>
+    </span>
 </p>

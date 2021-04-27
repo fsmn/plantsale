3d0a267f07e94289334e2a4bc9be86179e7757a1
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-26 20:04:29 -0500

    Issue #136 refactored all files in views/contact

--- src/application/views/contact/view.php
+++ src/application/views/contact/view.php
@@ -5,12 +5,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 // view.php Chris Dart Mar 5, 2015 2:26:16 PM chrisdart@cerebratorium.com
 ?>
 <div class="contact-info block">
-	<p><?php echo create_button(array("text"=>"Edit","href"=>site_url("contact/edit/$contact->id"),"class"=>array("button","small","edit","edit-contact")));?><label>&nbsp;Name: </label><?php echo $contact->name;?>
+	<p><?php print create_button([
+		'text' => 'Edit',
+		'href' => site_url('contact/edit/' . $contact->id),
+		'class' => [
+			'button',
+			'small',
+			'edit',
+			'edit-contact',
+		]]); ?><label>&nbsp;Name:
+		</label><?php print $contact->name; ?>
 	</p>
-	<p><label>Type: </label><?php echo ucfirst($contact->contact_type);?></p>
-	<p><label>Phone 1: </label><?php echo ucfirst(get_value($contact,"phone1_type","Main"));?>&nbsp;<?php echo $contact->phone1;?></p>
-	<p><label>Phone 2: </label><?php echo ucfirst(get_value($contact,"phone2_type","Main"));?>&nbsp;<?php echo $contact->phone2;?></p>
-	<p><label>Email: </label><?php echo get_value($contact,"email","");?></p>
-	<p><label>Notes: </label><?php echo get_value($contact,"notes");?></p>
-
+	<p><label>Type: </label><?php print ucfirst($contact->contact_type); ?></p>
+	<p><label>Phone 1:
+		</label><?php print ucfirst(get_value($contact, 'phone1_type', 'Main')); ?>&nbsp;<?php print $contact->phone1; ?></p>
+	<p><label>Phone 2:
+		</label><?php print ucfirst(get_value($contact, 'phone2_type', 'Main')); ?>&nbsp;<?php print $contact->phone2; ?></p>
+	<p><label>Email: </label><?php print get_value($contact, 'email', ''); ?></p>
+	<p><label>Notes: </label><?php print get_value($contact, 'notes'); ?></p>
 </div>

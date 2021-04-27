a95ebc1f5154e89d2fcc177064fc2b96f702e806
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 10:31:10 -0500

    Issue #136 refactored all files in views/logs

--- src/application/views/logs/view.php
+++ src/application/views/logs/view.php
@@ -1,25 +1,29 @@
 <?php
-if(!empty($rows) && !empty($types)):?>
-<ul class="filters">
-<?php foreach($types as $type):?>
-	<li class="filter-item"><a href="" data-target="<?php echo $type;?>"><?php echo $type; ?></a></li>
-<?php endforeach; ?>
-</ul>
-<table class="log-list">
-	<thead>
-	<tr>
-		<th>Type</th>
-		<th>Message</th>
-	</tr>
-	</thead>
-	<tbody>
-<?php foreach($rows as $row): ?>
-<?php [$type,$message] = $row;?>
-<tr class="file-output <?php print $type; ?>">
-	<td><?php echo $type;?></td>
-	<td><?php echo $message; ?></td>
-</tr>
-	<?php endforeach; ?>
-	</tbody>
-</table>
+if (!empty($rows) && !empty($types)) : ?>
+	<ul class="filters">
+		<?php foreach ($types as $type) : ?>
+			<li class="filter-item"><a href="" data-target="<?php print
+			 $type; ?>"><?php print
+			 $type; ?></a></li>
+		<?php endforeach; ?>
+	</ul>
+	<table class="log-list">
+		<thead>
+			<tr>
+				<th>Type</th>
+				<th>Message</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($rows as $row) : ?>
+				<?php [$type, $message] = $row; ?>
+				<tr class="file-output <?php print $type; ?>">
+					<td><?php print
+					 $type; ?></td>
+					<td><?php print
+					 $message; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
 <?php endif;

11c380a44a74386916a9af96d6beda28b19d6561
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 10:09:21 -0500

    Issue #136 refactored all files in views/image

--- src/application/views/image/view.php
+++ src/application/views/image/view.php
@@ -1,35 +1,34 @@
-<?php defined('BASEPATH') or exit ('No direct script access allowed');
-if (isset($variety)):
-	if (isset($message)): ?>
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+if (isset($variety)) :
+	if (isset($message)) : ?>
 		<div class="message alert"><?php print $message; ?></div>
 	<?php endif; ?>
-	<?php if (!get_value($variety, 'image_id', FALSE) && IS_EDITOR):
-	print create_button([
-			'text' => 'Add Image',
-			'class' => [
-				'new',
-				'button',
-				'small',
-				'add-image',
-			],
-			'id' => 'add-image_' . $variety->id,
-			'href' => base_url('variety/new_image/' . $variety->id),
-		]
-	);
+	<?php if (!get_value($variety, 'image_id', FALSE) && IS_EDITOR) :
+		print create_button(
+			[
+				'text' => 'Add Image',
+				'class' => [
+					'new',
+					'button',
+					'small',
+					'add-image',
+				],
+				'id' => 'add-image_' . $variety->id,
+				'href' => base_url('variety/new_image/' . $variety->id),
+			]
+		);
 	?>
-<?php else: ?>
-	<div class="center">
-		<img
-			src="https://nyc3.digitaloceanspaces.com/t7-live-fsmn/db.friendsschoolplantsale.com/files/<?php print $variety->id; ?>.jpg<?php print '?cache='. date('U');?>"
-			class="photo" alt="image of <?php print $variety->common_name; ?> "/>
-		<?php if (IS_EDITOR): ?>
-			<?php echo create_button([
-				'text' => 'Delete Image',
-				'class' => ['button', 'delete', 'delete-image'],
-				'id' => 'delete-image',
-				'data_values' => ['id' => $variety->image_id],
-			]); ?>
-		<?php endif; ?>
-	</div>
-<?php endif; ?>
+	<?php else : ?>
+		<div class="center">
+			<img src="https://nyc3.digitaloceanspaces.com/t7-live-fsmn/db.friendsschoolplantsale.com/files/<?php print $variety->id; ?>.jpg<?php print '?cache=' . date('U'); ?>" class="photo" alt="image of <?php print $variety->common_name; ?> " />
+			<?php if (IS_EDITOR) : ?>
+				<?php print create_button([
+					'text' => 'Delete Image',
+					'class' => ['button', 'delete', 'delete-image'],
+					'id' => 'delete-image',
+					'data_values' => ['id' => $variety->image_id],
+				]); ?>
+			<?php endif; ?>
+		</div>
+	<?php endif; ?>
 <?php endif;

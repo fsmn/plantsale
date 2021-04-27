f89cf3bfe9dcff637ec5aae0664071dea3ddbf85
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 09:32:48 -0500

    Issue #136 refactored all files in views/flag

--- src/application/views/flag/list.php
+++ src/application/views/flag/list.php
@@ -1,12 +1,12 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // list.php Chris Dart Apr 2, 2013 12:47:39 PM chrisdart@cerebratorium.com
-$delete_button = "";
-if(IS_EDITOR){
-	$delete_button = "<span class='button delete small flag-delete' id='flag-delete_%s'>Delete</span>";
+$delete_button = '';
+if (IS_EDITOR) {
+	$delete_button = '<span class="button delete small flag-delete" id="flag-delete_%s">Delete</span>';
 }
-$output = array();
-foreach($flags as $flag){
-	$output[] = sprintf("<div class='flag-row field-set' id='flag_%s'><img src='/images/%s'/>&nbsp;%s&nbsp;%s</div>",  $flag->id, $flag->source, $flag->name, sprintf($delete_button,$flag->id));
+$output = [];
+foreach ($flags as $flag) {
+	$output[] = sprintf('<div class="flag-row field-set" id="flag_%s"><img src="/images/%s"/>&nbsp;%s&nbsp;%s</div>',  $flag->id, $flag->source, $flag->name, sprintf($delete_button, $flag->id));
 }
-print implode("\r",$output);
+print implode("\r", $output);

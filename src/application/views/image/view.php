cfceae2a8fa6ef01d84c600abe89ff40cc1fb4b0
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 10:09:21 -0500

    Issue #136 refactored all files in views/image

--- src/application/views/image/view.php
+++ src/application/views/image/view.php
@@ -1,21 +1,22 @@
-<?php defined('BASEPATH') or exit ('No direct script access allowed');
-if (isset($variety)):
-	if (isset($message)): ?>
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+if (isset($variety)) :
+	if (isset($message)) : ?>
 		<div class="message alert"><?php print $message; ?></div>
 	<?php endif; ?>
-	<?php if (!get_value($variety, 'image_id', FALSE) && IS_EDITOR):
-	print create_button([
-			'text' => 'Add Image',
-			'class' => [
-				'new',
-				'button',
-				'small',
-				'add-image',
-			],
-			'id' => 'add-image_' . $variety->id,
-			'href' => base_url('variety/new_image/' . $variety->id),
-		]
-	);
+	<?php if (!get_value($variety, 'image_id', FALSE) && IS_EDITOR) :
+		print create_button(
+			[
+				'text' => 'Add Image',
+				'class' => [
+					'new',
+					'button',
+					'small',
+					'add-image',
+				],
+				'id' => 'add-image_' . $variety->id,
+				'href' => base_url('variety/new_image/' . $variety->id),
+			]
+		);
 	?>
 <?php else: ?>
 	<div class="center">

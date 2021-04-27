aeb64c98ae8b39a6b9e9dc0f793333a7749475c9
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 10:04:23 -0500

    Issue #136 refactored all files in views/grower/

--- src/application/views/grower/filter.php
+++ src/application/views/grower/filter.php
@@ -1,7 +1,8 @@
 <?php
 ?>
-<form name="grower-filter" action="<?php print base_url('grower/totals/');?>" method="get">
+<form name="grower-filter" action="<?php print base_url('grower/totals/'); ?>" method="get">
 	<div class="field-set label-break"><label for="year">Enter the year you want to view grower totals for</label>
-	<input type="number" name="year" value="<?php print get_current_year();?>"/></div>
-	<input type="submit" value="Filter" class="button btn btn-default"/>
+		<input type="number" name="year" value="<?php print get_current_year(); ?>" />
+	</div>
+	<input type="submit" value="Filter" class="button btn btn-default" />
 </form>

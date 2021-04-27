fdcae50faebc229552306be880eee6c227404c0d
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 09:28:17 -0500

    Issue #136 refactored all files in views/export

--- src/application/views/export/quark/categories.php
+++ src/application/views/export/quark/categories.php
@@ -1,38 +1,30 @@
 <?php
 
-defined('BASEPATH') or exit ('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 ?>
 <div class="categories-list">
 	<h5>Click on a category or subcategory to download the quark export for the
 		current year.</h5>
-	<?php echo create_button([
+	<?php print create_button([
 		'text' => 'Hide',
 		'class' => 'button hide-quark-export small',
 		'href' => '#',
 	]); ?>
 
 	<ul class="categories list">
-		<li class="category item"><a class="export"
-																 href="<?php echo site_url('index/quark'); ?>" target="_blank">All
+		<li class="category item"><a class="export" href="<?php print site_url('index/quark'); ?>" target="_blank">All
 				Categories <i class="fa fa-download"></i></a></li>
-		<?php foreach ($categories as $category): ?>
-			<li class="category item"><a class="export dialog"
-																	 href="<?php echo site_url('index/quark?category_id=' . $category->id); ?>"
-																	 title="Download all <?php echo $category->category; ?>"
-																	 target="_blank">
-					<?php echo $category->category; ?> <i class="fa fa-download"></i>
+		<?php foreach ($categories as $category) : ?>
+			<li class="category item"><a class="export dialog" href="<?php print site_url('index/quark?category_id=' . $category->id); ?>" title="Download all <?php print $category->category; ?>" target="_blank">
+					<?php print $category->category; ?> <i class="fa fa-download"></i>
 				</a></li>
-			<?php if (!empty($category->subcategories)): ?>
+			<?php if (!empty($category->subcategories)) : ?>
 				<li>
 					<ul class="subcategories list">
-						<?php foreach ($category->subcategories as $subcategory): ?>
-							<li class="subcategory item"><a class="export dialog"
-																							href="<?php echo site_url('index/quark?category_id='. $category->id . '& subcategory_id=' . $subcategory->id); ?>"
-																							title="Download ONLY <?php echo $subcategory->subcategory; ?>"
-																							target="_blank">
-									<?php echo $subcategory->subcategory; ?> <i
-										class='fa fa-download'></i>
+						<?php foreach ($category->subcategories as $subcategory) : ?>
+							<li class="subcategory item"><a class="export dialog" href="<?php print site_url('index/quark?category_id=' . $category->id . '& subcategory_id=' . $subcategory->id); ?>" title="Download ONLY <?php print $subcategory->subcategory; ?>" target="_blank">
+									<?php print $subcategory->subcategory; ?> <i class='fa fa-download'></i>
 								</a></li>
 						<?php endforeach; ?>
 					</ul>
--- src/application/views/export/quark/index.php
+++ src/application/views/export/quark/index.php
@@ -1,7 +1,6 @@
-<?php  defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 ?>
 
 <p>You can download the file here:</p>
-<p><a href="<?php echo $s3_path. $quark_path;?>"><?php echo $filename;?></a></p>
-
+<p><a href="<?php print $s3_path . $quark_path; ?>"><?php print $filename; ?></a></p>

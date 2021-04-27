2d7537f7113be5e52f810c351a361b565f9a9c64
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-26 19:18:43 -0500

    Issue #136 refactored all files in view/auth/email

--- src/application/views/auth/email/activate.tpl.php
+++ src/application/views/auth/email/activate.tpl.php
@@ -1,6 +1,7 @@
 <html>
 <body>
-	<h1><?php echo sprintf("Activate account for %s", $identity);?></h1>
-	<p><?php echo sprintf("Please click this link to %s.", anchor('auth/activate/'. $id .'/'. $activation, "activate"));?></p>
+	<h1><?php print sprintf('Activate account for %s', $identity); ?></h1>
+	<p><?php print sprintf('Please click this link to %s.', anchor('auth/activate/' . $id . '/' . $activation, 'activate')); ?>
+	</p>
 </body>
 </html>

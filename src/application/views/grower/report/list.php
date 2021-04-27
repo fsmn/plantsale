b393788c7bc1fb9d0ed11e87960357b32b602608
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 09:43:46 -0500

    Issue #136 refactored all files in views/grower/report

--- src/application/views/grower/report/list.php
+++ src/application/views/grower/report/list.php
@@ -1,33 +1,33 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // report.php Chris Dart Jan 8, 2015 2:58:21 PM chrisdart@cerebratorium.com
 //$grand_total = 0;
 ?>
-<h1>Grower Totals, <?php echo $year; ?></h1>
+<h1>Grower Totals, <?php print $year; ?></h1>
 <?php
 $buttons[] = [
 	'text' => 'Filter',
-	'class' => ['button', 'dialog','search'],
+	'class' => ['button', 'dialog', 'search'],
 	'style' => 'default',
 	'title' => 'Filter by year',
 	'href' => base_url('grower/filter'),
 ];
 $buttons[] = [
-	"text" => "Export",
-	"class" => ["button", "export"],
-	"style" => "export",
-	"title" => "Export as Spreadsheet",
-	"href" => $_SERVER['REQUEST_URI'] . "?export=true",
+	'text' => 'Export',
+	'class' => ['button', 'export'],
+	'style' => 'export',
+	'title' => 'Export as Spreadsheet',
+	'href' => $_SERVER['REQUEST_URI'] . '?export=true',
 ]; ?>
 <?php if (isset($orphan_count) && $orphan_count > 0) {
-	$verb = $orphan_count == 1 ? "is" : "are";
-	$plural = $orphan_count > 1 ? "s" : "";
+	$verb = $orphan_count == 1 ? 'is' : 'are';
+	$plural = $orphan_count > 1 ? 's' : '';
 	$buttons[] = [
-		"text" => "Show $orphan_count Orphan Grower$plural <span class='badge'>$orphan_count</span>",
-		"title" => "There $verb $orphan_count grower$plural with no grower record. Click here to fix this.",
-		"class" => ["button"],
-		"style" => "default",
-		"href" => site_url("grower/show_orphans"),
+		'text' => "Show $orphan_count Orphan Grower$plural <span class='badge'>$orphan_count</span>",
+		'title' => "There $verb $orphan_count grower$plural with no grower record. Click here to fix this.",
+		'class' => ['button'],
+		'style' => 'default',
+		'href' => site_url('grower/show_orphans'),
 	];
 }
 
@@ -35,40 +35,40 @@ print create_button_bar($buttons);
 ?>
 <table class="list">
 	<thead>
-	<tr>
-		<th>
-			ID
-		</th>
-		<th>
-			Name
-		</th>
-		<th>
-			Email/Phone
-		</th>
-		<th>
-			Address
-		</th>
-		<th>
-			Our Contact
-		</th>
-		<th>
-			Totals for <?php echo $year; ?>
-		</th>
-		<th class="no-print">
-		</th>
-	</tr>
+		<tr>
+			<th>
+				ID
+			</th>
+			<th>
+				Name
+			</th>
+			<th>
+				Email/Phone
+			</th>
+			<th>
+				Address
+			</th>
+			<th>
+				Our Contact
+			</th>
+			<th>
+				Totals for <?php print $year; ?>
+			</th>
+			<th class="no-print">
+			</th>
+		</tr>
 	</thead>
 	<tbody>
-	<?php
-	print implode("\r", $growers); ?>
+		<?php
+		print implode("\r", $growers); ?>
 	</tbody>
 	<tfoot>
-	<tr>
-		<th colspan="5" style="text-align: right;">
-			<?php echo get_as_price($grand_total); ?>
-		</th>
-		<th>
-		</th>
-	</tr>
+		<tr>
+			<th colspan="5" style="text-align: right;">
+				<?php print get_as_price($grand_total); ?>
+			</th>
+			<th>
+			</th>
+		</tr>
 	</tfoot>
 </table>
--- src/application/views/grower/report/row.php
+++ src/application/views/grower/report/row.php
@@ -3,31 +3,30 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 // row.php Chris Dart Jan 8, 2015 2:59:57 PM chrisdart@cerebratorium.com
-$email_phone = array();
-if($grower->email){
-    $email_phone[] = format_email($grower,'email');
+$email_phone = [];
+if ($grower->email) {
+	$email_phone[] = format_email($grower, 'email');
 }
-if($grower->phone){
-    $email_phone[] = $grower->phone;
+if ($grower->phone) {
+	$email_phone[] = $grower->phone;
 }
-if($grower->fax){
-    $email_phone[] = $grower->fax;
+if ($grower->fax) {
+	$email_phone[] = $grower->fax;
 }
-
-
 ?>
 <tr class="row grower total">
-<td class="field"><a href="<?php echo base_url('grower/view/' . $grower->id);?>" title="view and edit the grower record"><?php echo $grower->id;?></a></td>
-	<td class="field"><a href="<?php echo base_url('grower/view/' . $grower->id);?>' title="View and edit the grower record"><?php echo $grower->grower_name;?></a></td>
+	<td class="field"><a href="<?php print base_url('grower/view/' . $grower->id); ?>" title="view and edit the grower record"><?php print $grower->id; ?></a></td>
+	<td class="field"><a href="<?php print base_url('grower/view/' . $grower->id); ?>' title=" View and edit the grower record"><?php print $grower->grower_name; ?></a></td>
 	<td class="field">
-	<?php echo implode("<br/>",$email_phone);?>
+		<?php print implode('<br/>', $email_phone); ?>
 	</td>
 	<?php $address = format_address($grower); ?>
-	<td class="field"><?php echo $address['street'];?><br/>
-	<?php echo $address['locale'];
-	echo $address['country'] != 'USA'? ', ' . $address['country']:'';
-	?></td><td>
-	<?php echo $grower->first_name . ' ' . $grower->last_name; ?>
-		<td class="field" style="text-align: right;"><?php echo get_as_price($grower->total);?></td>
-		<td class="field no-print"><a href="<?php echo base_url('order/search?find=true&grower_id=' . $grower->id . '&year='. $year .'&sorting%5B%5D=genus&direction%5B%5D=ASC');?>" title="View current orders for this grower"><?php echo $year; ?> Orders</a></td>
+	<td class="field"><?php print $address['street']; ?><br />
+		<?php print $address['locale'];
+		print $address['country'] != 'USA' ? ', ' . $address['country'] : '';
+		?></td>
+	<td>
+		<?php print $grower->first_name . ' ' . $grower->last_name; ?>
+	<td class="field" style="text-align: right;"><?php print get_as_price($grower->total); ?></td>
+	<td class="field no-print"><a href="<?php print base_url('order/search?find=true&grower_id=' . $grower->id . '&year=' . $year . '&sorting%5B%5D=genus&direction%5B%5D=ASC'); ?>" title="View current orders for this grower"><?php print $year; ?> Orders</a></td>
 </tr>

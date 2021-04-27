5941f0346b5c22d738045cd1fffc75f2cea5a9d7
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-27 09:32:48 -0500

    Issue #136 refactored all files in views/flag

--- src/application/views/flag/edit.php
+++ src/application/views/flag/edit.php
@@ -1,9 +1,9 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // edit.php Chris Dart Apr 2, 2013 12:47:50 PM chrisdart@cerebratorium.com
 
 ?>
 
 <div class="flag-row field-set">
-<?php echo form_dropdown("name",$flags,"","id='name' class='flag-insert'");?>
+    <?php print form_dropdown('name', $flags, '', 'id="name" class="flag-insert"'); ?>
 </div>

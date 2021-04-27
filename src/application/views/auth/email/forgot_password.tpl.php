2c8487a43a8b58d8533e09e486f7399d040c0e7c
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-26 19:18:43 -0500

    Issue #136 refactored all files in view/auth/email

--- src/application/views/auth/email/forgot_password.tpl.php
+++ src/application/views/auth/email/forgot_password.tpl.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-	<h1><?php echo sprintf("Reset Password for %s", $identity);?></h1>
-	<p><?php echo sprintf("Please click this link to %s.", anchor('auth/reset_password/'. $forgotten_password_code, "reset your password"));?></p>
+	<h1><?php print sprintf('Reset Password for %', $identity); ?></h1>
+	<p><?php print sprintf('Please click this link to %s.', anchor('auth/reset_password/' . $forgotten_password_code, 'reset your password')); ?></p>
 </body>
 </html>

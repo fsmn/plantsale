093898ba864c370bae0f540be822df2783cd10f6
lnewmanheggie <lnewmanheggie@gmail.com>
2021-05-02 12:09:37 -0500

    Issue #136 refactored all files in views/utility

--- src/application/views/utility/sale_year.php
+++ src/application/views/utility/sale_year.php
@@ -1,10 +1,9 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<form name="set_sale_year" id="set_sale_year" action="<?php echo base_url("index/set_year");?>" method="get">
-<input type="hidden" name="uri" id="uri" value="<?php echo $uri;?>"/>
-<input type="text" name="sale_year" id="sale_year" style="width:5em;" value="<?php echo get_current_year();?>"/>
-<input type="submit" class='btn btn-default edit button' value="update"/>
+<form name="set_sale_year" id="set_sale_year" action="<?php print base_url('index/set_year'); ?>" method="get">
+    <input type="hidden" name="uri" id="uri" value="<?php print $uri; ?>" />
+    <input type="text" name="sale_year" id="sale_year" style="width:5em;" value="<?php print get_current_year(); ?>" />
+    <input type="submit" class='btn btn-default edit button' value="update" />
 
 </form>

1e6262c3ede45483e1397f11308e87f152613cd1
lnewmanheggie <lnewmanheggie@gmail.com>
2021-05-05 11:57:14 -0500

    Issue #139 converted sprintf to format_string in views/auth/email/forgot_password.tpl

--- src/application/views/auth/email/forgot_password.tpl.php
+++ src/application/views/auth/email/forgot_password.tpl.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-	<h1><?php print sprintf('Reset Password for %', $identity); ?></h1>
-	<p><?php print sprintf('Please click this link to %s.', anchor('auth/reset_password/' . $forgotten_password_code, 'reset your password')); ?></p>
+	<h1><?php print format_string('Reset Password for @identity', ['@identity' => $identity]); ?></h1>
+	<p><?php print format_string('Please click this link to @url.', ['@url' => anchor('auth/reset_password/' . $forgotten_password_code, 'reset your password')]); ?></p>
 </body>
 </html>

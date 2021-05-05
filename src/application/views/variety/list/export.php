7f918e6b26f10389aea695d9878a26cacafc59a4
lnewmanheggie <lnewmanheggie@gmail.com>
2021-05-05 15:17:30 -0500

    Issue #139 converted sprintf to format_string in views/variety/list/list

--- src/application/views/variety/list/export.php
+++ src/application/views/variety/list/export.php
@@ -1,60 +1,60 @@
 <?php
-defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
-if(!empty($plants)) {
+defined('BASEPATH') or exit('No direct script access allowed');
+if (!empty($plants)) {
 	if (empty($filename)) {
 		$filename = 'varieties.csv';
 	}
-	if(empty($export_type)){
+	if (empty($export_type)) {
 		$export_type = 'standard';
 	}
 	$handle = fopen('php://output', 'w');
-	header("Content-type: application/csv");
-	header("Content-Disposition: attachment; filename=" . $filename);
-	header("Pragma: no-cache");
-	header("Expires: 0");// Define the fields desired for output in this array
+	header('Content-type: application/csv');
+	header('Content-Disposition: attachment; filename=' . $filename);
+	header('Pragma: no-cache');
+	header('Expires: 0'); // Define the fields desired for output in this array
 	$fields = [
-		"year" => "Year",
-		"catalog_number" => "Catalog Number",
-		"new_year" => "First year at Sale",
-		"name" => "Common Name",
-		"genus" => "Genus",
-		"species" => "Species",
-		"variety" => "Variety",
-		"category" => "Category",
-		"subcategory" => "Subcategory",
-		"description" => "Description",
-		"print_description" => "Print Description",
-		"web_description" => "Web Description",
-		"plant_color" => "Plant Color",
-		"min_height" => "Min Height",
-		"max_height" => "Max Height",
-		"height_unit" => "Height Unit",
-		"min_width" => "Min Width",
-		"max_width" => "Max Width",
-		"width_unit" => "Width Unit",
-		"grower_id" => "Grower ID",
-		"null_link" => "Link",
+		'year' => 'Year',
+		'catalog_number' => 'Catalog Number',
+		'new_year' => 'First year at Sale',
+		'name' => 'Common Name',
+		'genus' => 'Genus',
+		'species' => 'Species',
+		'variety' => 'Variety',
+		'category' => 'Category',
+		'subcategory' => 'Subcategory',
+		'description' => 'Description',
+		'print_description' => 'Print Description',
+		'web_description' => 'Web Description',
+		'plant_color' => 'Plant Color',
+		'min_height' => 'Min Height',
+		'max_height' => 'Max Height',
+		'height_unit' => 'Height Unit',
+		'min_width' => 'Min Width',
+		'max_width' => 'Max Width',
+		'width_unit' => 'Width Unit',
+		'grower_id' => 'Grower ID',
+		'null_link' => 'Link',
 	];
 
-	if ($export_type == "copy_edits") {
+	if ($export_type == 'copy_edits') {
 		$fields = [
-			"name" => "Common Name",
-			"genus" => "Genus",
-			"species" => "Species",
-			"variety" => "Variety",
-			"category" => "Category",
-			"subcategory" => "Subcategory",
-			"grower_id" => "Grower ID",
-			"new_year" => "New",
-			"null_writer" => "Writer",
-			"null_coordinator" => "Coordinator",
-			"null_copy_in" => "Copy in DB",
-			"null_copy_recd" => "Copy Received",
-			"null_notes" => "Notes",
-			"description" => "Description",
-			"print_description" => "Print Description",
-			"web_description" => "Web Description",
-			"null_link" => "Link",
+			'name' => 'Common Name',
+			'genus' => 'Genus',
+			'species' => 'Species',
+			'variety' => 'Variety',
+			'category' => 'Category',
+			'subcategory' => 'Subcategory',
+			'grower_id' => 'Grower ID',
+			'new_year' => 'New',
+			'null_writer' => 'Writer',
+			'null_coordinator' => 'Coordinator',
+			'null_copy_in' => 'Copy in DB',
+			'null_copy_recd' => 'Copy Received',
+			'null_notes' => 'Notes',
+			'description' => 'Description',
+			'print_description' => 'Print Description',
+			'web_description' => 'Web Description',
+			'null_link' => 'Link',
 		];
 	}
 
@@ -63,27 +63,25 @@ if(!empty($plants)) {
 	fputcsv($handle, $header_values);
 	foreach ($plants as $plant) {
 		$current_year = $plant->year;
-    $line = [];
+		$line = [];
 		foreach (array_keys($fields) as $key) {
-			if ($key == "new_year") {
+			if ($key == 'new_year') {
 				if ($plant->$key == $current_year) {
-					$line [] = "New";
+					$line[] = 'New';
+				} else {
+					$line[] = '';
 				}
-				else {
-					$line [] = "";
-				}
-			}
-			elseif (stristr($key, "null_")) {
-				$line [] = "";
-			}
-			else {
-				$line [] = $plant->$key;
+			} elseif (stristr($key, 'null_')) {
+				$line[] = '';
+			} else {
+				$line[] = $plant->$key;
 			}
 		}
-		$line[] = sprintf("http://db.friendsschoolplantsale.com/variety/view/%s", $plant->id);
+		$line[] = format_string('http://db.friendsschoolplantsale.com/variety/view/@id', [
+			'@id' => $plant->id
+		]);
 
 		fputcsv($handle, $line);
-
 	}
 
 	fclose($handle);
--- src/application/views/variety/list/history.php
+++ src/application/views/variety/list/history.php
@@ -1,27 +1,22 @@
-<?php defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$this->load->view("variety/list/header");
+$this->load->view('variety/list/header');
 ?>
 
 
-<?php foreach($plants as $plant):?>
-<div id="variety-row_<?php echo $plant->id;?>" style="padding-top:2em;">
+<?php foreach ($plants as $plant) : ?>
+	<div id="variety-row_<?php print $plant->id; ?>" style="padding-top:2em;">
 
-<div id="variety-info">
-		<h4><span><a
-			href="<?php echo site_url(sprintf("common/find?genus=%s",$plant->genus));?>"
-			title="View all <?php echo $plant->genus;?>"><?php echo $plant->genus;?></a></span>&nbsp;
-		<span><?php echo $plant->species;?></span>&nbsp;
-		<span><a href="<?php echo site_url("common/view/$plant->common_id");?>"
-			title="View the details for <?php echo $plant->name;?>"><?php echo $plant->name;?></a></span>&nbsp;
-		<span><a href="<?php echo site_url("variety/view/$plant->id");?>"
-			title="View the details for <?php echo $plant->variety;?>"><?php echo $plant->variety;?></a></span>
+		<div id="variety-info">
+			<h4><span><a href="<?php print site_url(format_string('common/find?genus=@genus', ['@genus' => $plant->genus])); ?>" title="View all <?php print $plant->genus; ?>"><?php print $plant->genus; ?></a></span>&nbsp;
+				<span><?php print $plant->species; ?></span>&nbsp;
+				<span><a href="<?php print site_url('common/view/' . $plant->common_id); ?>" title="View the details for <?php print $plant->name; ?>"><?php print $plant->name; ?></a></span>&nbsp;
+				<span><a href="<?php print site_url('variety/view/' . $plant->id); ?>" title="View the details for <?php print $plant->variety; ?>"><?php print $plant->variety; ?></a></span>
 			</h4>
+		</div>
+		<div id="variety-orders" style="max-width: 900px;">
+		<?php $data['orders'] = $plant->orders;
+		$this->load->view('order/list', $data);
+	endforeach; ?>
+		</div>
 	</div>
-<div id="variety-orders" style="max-width: 900px;">
-<?php $data["orders"] = $plant->orders;
-$this->load->view("order/list",$data);
-endforeach; ?>
-</div>
-</div>
-

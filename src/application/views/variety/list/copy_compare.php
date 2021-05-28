67a7d534091df62c39dc8ce1b289ca55ea907d81
lnewmanheggie <lnewmanheggie@gmail.com>
2021-05-27 21:22:58 -0500

    refactored files in view/variety

--- src/application/views/variety/list/copy_compare.php
+++ src/application/views/variety/list/copy_compare.php
@@ -1,64 +1,69 @@
 <?php
-$fields = array (
-		"Common",
-		"Latin",
-		"Variety",
-		"Description",
-		"ARCHIVED Description",
-		"Web Description",
-		"ARCHIVED Variety Description",
-		"Variety Description",
-		"ARCHIVED Extended Description" 
-);
+$fields = [
+		'Common',
+		'Latin',
+		'Variety',
+		'Description',
+		'ARCHIVED Description',
+		'Web Description',
+		'ARCHIVED Variety Description',
+		'Variety Description',
+		'ARCHIVED Extended Description'
+];
 
 ?>
-<h1>Total Count: <?php echo count($plants); ?></h1>
+<h1>Total Count: <?php print count($plants); ?></h1>
 <table class="list table">
-<thead>
-<tr>
-<?php foreach($fields as $field):?>
-<th><?php echo $field; ?>
-<?php endforeach;?>
-</tr>
-</thead>
-<tbody>
-<?php foreach($plants as $plant):?>
-<tr>
-<td><a href="<?php echo base_url("common/view/$plant->common_id");?>"><?php echo $plant->name;?></a></td>
-<td><?php echo format_latin_name($plant);?></td>
-<td><a href="<?php echo base_url("variety/view/$plant->variety_id");?>"><?php echo $plant->variety;?></a></td>
-<td>
-<span class="field-envelope" id="common__description__<?php echo $plant->common_id;?>">
+	<thead>
+	<tr>
+		<?php foreach ($fields as $field): ?>
+		<th><?php print $field; ?>
+			<?php endforeach; ?>
+	</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($plants as $plant): ?>
+		<tr>
+			<td><a href="<?php print base_url('common/view/' . $plant->common_id); ?>"><?php print $plant->name; ?></a></td>
+			<td><?php print format_latin_name($plant); ?></td>
+			<td><a href="<?php print base_url('variety/view/' . $plant->variety_id); ?>"><?php print $plant->variety; ?></a>
+			</td>
+			<td>
+				<span class="field-envelope" id="common__description__<?php print $plant->common_id; ?>">
 				<span class="textarea live-field text" name="description">
-					<textarea name="description" cols="40" rows="10" id="description_<?php echo $plant->common_id;?>" size="127" type="textarea" category=""><?php echo get_value($plant,"description");?></textarea>
+					<textarea name="description" cols="40" rows="10" id="description_<?php print $plant->common_id; ?>"
+							  size="127" type="textarea"
+							  category=""><?php print get_value($plant, 'description'); ?></textarea>
 				</span>
-			</span>
+				</span>
 			</td>
 			<td>
-<?php echo $plant->old_description; ?>
-</td>
-			<td><span class="field-envelope" id="variety__web_description__<?php echo $plant->variety_id?>">
+				<?php print $plant->old_description; ?>
+			</td>
+			<td><span class="field-envelope" id="variety__web_description__<?php print $plant->variety_id ?>">
 				<span class="textarea live-field text" name="web_description">
-					<textarea name="web_description" cols="40" rows="10" id="web-description_<?php echo $plant->variety_id;?>" size="5" type="textarea" category=""><?php echo get_value($plant,"web_description");?></textarea>
+					<textarea name="web_description" cols="40" rows="10"
+							  id="web-description_<?php print $plant->variety_id; ?>" size="5" type="textarea"
+							  category=""><?php print get_value($plant, 'web_description'); ?></textarea>
 				</span>
 			</span>
 			</td>
 			<td>
-<?php echo $plant->old_print_description;?>
-</td>
-<td>
-<span class="field-envelope" id="variety__print_description__<?php echo $plant->variety_id?>">
+				<?php print $plant->old_print_description; ?>
+			</td>
+			<td>
+				<span class="field-envelope" id="variety__print_description__<?php print $plant->variety_id ?>">
 				<span class="textarea live-field text" name="print_description">
-					<textarea name="print_description" cols="40" rows="10" id="print-description_<?php echo $plant->variety_id;?>" size="5" type="textarea" category=""><?php echo get_value($plant,"print_description");?></textarea>
+					<textarea name="print_description" cols="40" rows="10"
+							  id="print-description_<?php print $plant->variety_id; ?>" size="5" type="textarea"
+							  category=""><?php print get_value($plant, 'print_description'); ?></textarea>
 				</span>
 			</span>
-</td>
-
-
-<td>
-<?php echo $plant->extended_description;?>
-</td>
-</tr>
-<?php endforeach;?>
-</tbody>
+			</td>
+			<td>
+				<?php print $plant->extended_description; ?>
+			</td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
 </table>
--- src/application/views/variety/list/flags.php
+++ src/application/views/variety/list/flags.php
@@ -1,15 +1,15 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // batch_flag_list.php Chris Dart Mar 2, 2015 2:19:06 PM chrisdart@cerebratorium.com
-$this->load->view("variety/list/header");
+$this->load->view('variety/list/header');
 foreach ($plants as $variety):?>
-	<h4><?php echo $variety->genus . "&nbsp;" . $variety->variety; ?>
-		(<?php echo format_latin_name($variety); ?>) <a
-				href="<?php echo site_url("variety/view/$variety->id"); ?>">View</a>
+	<h4><?php print $variety->genus . '&nbsp;' . $variety->variety; ?>
+		(<?php print format_latin_name($variety); ?>) <a
+				href="<?php print site_url('variety/view/' . $variety->id); ?>">View</a>
 	</h4>
-	<div class="flag-list" id="flag-list_<?php echo $variety->id; ?>">
-		<?php $data = ["flags" => $variety->flags]; ?>
-		<?php $this->load->view("flag/list", $data); ?>
+	<div class="flag-list" id="flag-list_<?php print $variety->id; ?>">
+		<?php $data = ['flags' => $variety->flags]; ?>
+		<?php $this->load->view('flag/list', $data); ?>
 	</div>
 
 	<?php if (IS_EDITOR): ?>
@@ -20,7 +20,7 @@ foreach ($plants as $variety):?>
 				'class' => 'button small new flag-add',
 				'id' => 'fa_' . $variety->id,
 		]; ?>
-		<?php echo create_button($flag_buttons); ?>
+		<?php print create_button($flag_buttons); ?>
 	<?php endif; ?>
 
 <?php endforeach;

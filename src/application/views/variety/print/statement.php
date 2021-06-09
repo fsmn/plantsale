8badadc1968e3eeefe1895e1d822a6ddfcc9e12e
Chris Dart <chris@ten7.com>
2021-06-09 13:26:02 -0500

    Issue #147 update printable variety poster image paths.

--- src/application/views/variety/print/statement.php
+++ src/application/views/variety/print/statement.php
@@ -62,7 +62,7 @@ $common_class = implode(" ", $common_classes);
 	<div class="description-group">
 	<?php if($has_image):?>
 	<div class="image">
-		<img src="https://nyc3.digitaloceanspaces.com/t7-live-fsmn/db.friendsschoolplantsale.com/files/<?php print $variety->id;?>.jpg<?php print '?cache='. date('U');?>" class="photo" alt="image of <?php print $variety->common_name; ?> "/>
+		<img src="/files/<?php print $variety->id;?>.jpg<?php print '?cache='. date('U');?>" class="photo" alt="image of <?php print $variety->common_name; ?> "/>
 </div>
 <?php endif;?>
 <div class="description-text">
